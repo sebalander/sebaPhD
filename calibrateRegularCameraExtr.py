405d4a2ae4a84bcb8cd02e38c4e6359caae0e6a3
sebalander <seba.arroyo7@gmail.com>
2016-07-22 17:29:11 -0300

    Successfull extr calibration of fisheye with rational model

--- calibrateRegularCameraExtr.py
+++ calibrateRegularCameraExtr.py
@@ -12,27 +12,129 @@ must provide initial conditions
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2.fisheye as fe
-import glob
+from lmfit import minimize, Parameters
+import pickle
 
-# %% LOAD DATA
+# %%
+def reform(params):
+    # get params into correct format
+    rvec = np.zeros((3,1))
+    tvec = np.zeros((3,1))
+    for i in range(3):
+        rvec[i,0] = params['rvec%d'%i].value
+        tvec[i,0] = params['tvec%d'%i].value
+    
+    cameraMatrix = np.zeros((3,3))  
+    cameraMatrix[0,0] = params['cameraMatrix0'].value
+    cameraMatrix[1,1] = params['cameraMatrix1'].value
+    cameraMatrix[0,2] = params['cameraMatrix2'].value
+    cameraMatrix[1,2] = params['cameraMatrix3'].value    
+    cameraMatrix[2,2] = 1
+
+    distCoeffs = np.zeros((14,1))
+    for i in range(14):
+        distCoeffs[i,0] = params['distCoeffs%d'%i].value 
+
+    return rvec, tvec, cameraMatrix, distCoeffs
+
+
+# %% FILES
 # input files
 imageFile = "/home/sebalander/code/sebaPhD/resources/fishGrid/FEsheetBoard1920pix.png"
 cornersFile = "/home/sebalander/code/sebaPhD/resources/fishGrid/FEsheetCorners.npy"
 patternFile = "./resources/fishGrid/FEsheetPattern.npy"
-rvecInitialFile = "./resources/fishGrid/FErvecInitial.npy"
-tvecInitialFile = "./resources/fishGrid/FEtvecInitial.npy"
+rvecInitialFile = "./resources/fishGrid/FEsheetRvecInitial.npy"
+tvecInitialFile = "./resources/fishGrid/FEsheetTvecInitial.npy"
 
 # intrinsic parameters (input)
 distCoeffsFile = "./resources/fishDistCoeffs.npy"
 linearCoeffsFile = "./resources/fishLinearCoeffs.npy"
 
-corners = np.load(cornersFile)
-chessboardModel = np.load(patternFile)
-distCoeffs = np.load(distCoeffsFile)
-linearCoeffs = np.load(linearCoeffsFile)
-
 # output files
 poseFile = "./resources/fishGrid/FEpose.npy"
 
+# %% LOAD DATA
+img = cv2.imread(imageFile)
+corners = np.load(cornersFile)
+objectPoints = np.load(patternFile)
+rVecIni = np.load(rvecInitialFile)
+tVecIni = np.load(tvecInitialFile)
+distCoeffs = np.load(distCoeffsFile)
+linearCoeffs = np.load(linearCoeffsFile)
 
+# %% map wit initial conditions
+projectedPointsIni, _ = cv2.projectPoints(objectPoints,
+                                          rVecIni,
+                                          tVecIni,
+                                          linearCoeffs,
+                                          distCoeffs)
+
+
+# %% PLOT FOR INITIAL CONDITIONS
+imagePntsX = projectedPointsIni[:,0,0]
+imagePntsY = projectedPointsIni[:,0,1]
+
+cornersX = corners[:,0,0]
+cornersY = corners[:,0,1]
+
+plt.imshow(img)
+plt.plot(cornersX, cornersY, 'xr', markersize=10)
+plt.plot(imagePntsX, imagePntsY, '+b', markersize=10)
+
+# %% DEFINE CUADRATIC ERROR ON THE IMAGE
+# http://cars9.uchicago.edu/software/python/lmfit/intro.html
+
+def residual(params, objectPoints, corners):
+    
+    rvec, tvec, cameraMatrix, distCoeffs = reform(params)
+    
+    # project points
+    projectedPoints, _ = cv2.projectPoints(objectPoints,
+                                     rvec,
+                                     tvec,
+                                     cameraMatrix,
+                                     distCoeffs)
+    
+    return corners[:,0,:] - projectedPoints[:,0,:]
+
+
+# %% PARAMETERS
+# problem, cant use a matrix or vector as parameter, will have to bypass this
+params = Parameters()
+for i in range(3):
+    params.add('rvec%d'%i, value=rVecIni[i,0], vary=True)
+    params.add('tvec%d'%i, value=tVecIni[i,0], vary=True)
+
+
+params.add('cameraMatrix0', value=linearCoeffs[0,0], vary=False)
+params.add('cameraMatrix1', value=linearCoeffs[1,1], vary=False)
+params.add('cameraMatrix2', value=linearCoeffs[0,2], vary=False)
+params.add('cameraMatrix3', value=linearCoeffs[1,2], vary=False)
+
+for i in range(14):
+    params.add('distCoeffs%d'%i, value=distCoeffs[i,0], vary=False)
+
+
+# %% OPTIMIZE
+out = minimize(residual, params, args=(objectPoints, corners))
+
+# %% OPTIMIZED PARAMETERS
+rVecOpt, tVecOpt, _ , _ = reform(out.params)
+
+# %% map with OPTIMAL conditions
+projectedPointsOut, _ = cv2.projectPoints(objectPoints,
+                                          rVecOpt,
+                                          tVecOpt,
+                                          linearCoeffs,
+                                          distCoeffs)
+
+# %% PLOT FOR FINAL CONDITIONS
+imagePntsX = projectedPointsOut[:,0,0]
+imagePntsY = projectedPointsOut[:,0,1]
+
+cornersX = corners[:,0,0]
+cornersY = corners[:,0,1]
+
+plt.imshow(img)
+plt.plot(cornersX, cornersY, 'xr', markersize=10)
+plt.plot(imagePntsX, imagePntsY, '+b', markersize=10)
--- resources/fishGrid/calcInitialCond.py
+++ resources/fishGrid/calcInitialCond.py
@@ -9,11 +9,14 @@ generate the camera's pose initial conditions by hand
 import cv2
 import numpy as np
 import numpy.linalg as lin
-import matplotlib.pyplot as plt
 from scipy.linalg import sqrtm, inv
 
+# %% 
+tVecFile = "./resources/fishGrid/FEsheetTvecInitial.npy"
+rVecFile = "./resources/fishGrid/FEsheetRvecInitial.npy"
+
 # %% INITIAL TRASLATION VECTOR
-tvecInitial = np.array([0,0,2.7]) 
+tVec = np.array([[0], [0], [2.7]]) 
 
 # %% ROTATION MATRIX
 z = np.array([0, 1, -2.7])
@@ -24,13 +27,18 @@ x = np.array([-8*21, 5*29.7, 0])
 y = np.array([5*21, 5*29.7, 0])
 
 # hacer que x,y sean perp a z, agregar la tercera componente 
-x = x - z * np.dot(x,z)
+x = x - z * np.dot(x,z) # hago perpendicular a z
 x /= lin.norm(x) 
 
 y = y - z * np.dot(y,z) # hago perpendicular a z
 y /= lin.norm(y)
 
-# make into versor matrix
+# %% test ortogonal
+np.dot(x,z)
+np.dot(x,y)
+np.dot(y,z)
+
+# %% make into versor matrix
 rMatrix = np.array([x,y,z])
 
 # find nearest ortogonal matrix
@@ -39,9 +47,14 @@ rMatrix = rMatrix.dot(inv(sqrtm(rMatrix.T.dot(rMatrix))))
 
 # %% SAVE PARAMETERS
 
+# convert to rodrigues vector
+rVec, _ = cv2.Rodrigues(rMatrix)
+
+np.save(tVecFile, tVec)
+np.save(rVecFile, rVec)
 
 # %% PLOT VECTORS
-[x,y,z] = rMatrix
+[x,y,z] = rMatrix # get from ortogonal matrix
 
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
