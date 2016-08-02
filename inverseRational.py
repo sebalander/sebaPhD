Almost fixed inverseRational, plotted points in 3D It still returns bad results, check calculations (rotation marix?)

inverseRational.py
@@ -11,47 +11,53 @@ import numpy as np
 
 
 # %%
-def inverseRational(u, v, cameraMatrix, distCoeffs):
+def inverseRational(corners, cameraMatrix, distCoeffs, rotMatrix, tVec):
     '''
-    inverseRational(u, v, cameraMatrix, distCoeffs) -> X,Y
-    takes position (u,v) in image and returns (X,Y,0) in scene
+    inverseRational(corners, cameraMatrix, distCoeffs) -> X,Y
+    takes corners in image and returns (X,Y,0) in scene
+    corners must be of size (n,1,2)
     '''
-    xpp = (u-cameraMatrix[0,2]) / cameraMatrix[0,0]
-    ypp = (v-cameraMatrix[1,2]) / cameraMatrix[1,1]
+    xpp = (corners[:,0,0]-cameraMatrix[0,2]) / cameraMatrix[0,0]
+    ypp = (corners[:,0,1]-cameraMatrix[1,2]) / cameraMatrix[1,1]
     rpp = np.sqrt(xpp**2 + ypp**2)
     
     # polynomial coeffs
     # # (k1,k2,p1,p2[,k3[,k4,k5,k6[,s1,s2,s3,s4[,τx,τy]]]])
-    p = [distCoeffs[4,0],
-         -rpp*distCoeffs[7,0],
-         distCoeffs[1,0],
-         -rpp*distCoeffs[6,0],
-         distCoeffs[0,0],
-         -rpp*distCoeffs[5,0],
-         1,
-         -rpp]
+    poly = [[distCoeffs[4,0],
+             -r*distCoeffs[7,0],
+             distCoeffs[1,0],
+             -r*distCoeffs[6,0],
+             distCoeffs[0,0],
+             -r*distCoeffs[5,0],
+             1,
+             -r] for r in rpp]
     
-    roots = np.roots(p)
-    
+    # roots = np.roots(p)
+    roots = [np.roots(p) for p in poly]
 #    # max radious possible
 #    rppMax = np.sqrt((cameraMatrix[0,2] / cameraMatrix[0,0])**2 +
 #                     (cameraMatrix[1,2] / cameraMatrix[1,1])**2)
     
-    rp_rpp = roots[np.isreal(roots)]/rpp # asume real positive root, in interval
+    # rp_rpp = roots[np.isreal(roots)].real[0] / rpp # asume real positive root, in interval
+    rp_rpp = np.array([roo[np.isreal(roo)].real[0] for roo in roots]) / rpp
     
-    xp = xpp*rp_rpp
-    yp = ypp*rp_rpp
+    xp = xpp * rp_rpp
+    yp = ypp * rp_rpp
     
     # auxiliar calculations
-    a = cameraMatrix[0,0] - cameraMatrix[2,0] * xp
-    b = cameraMatrix[0,1] - cameraMatrix[2,1] * xp
-    c = cameraMatrix[0,2] - cameraMatrix[2,2] * xp
-    d = cameraMatrix[1,0] - cameraMatrix[2,0] * yp
-    e = cameraMatrix[1,1] - cameraMatrix[2,1] * yp
-    f = cameraMatrix[1,2] - cameraMatrix[2,2] * yp
+    a = rotMatrix[0,0] - rotMatrix[2,0] * xp
+    b = rotMatrix[0,1] - rotMatrix[2,1] * xp
+    c = tVec[0,0] - tVec[2,0] * xp
+    d = rotMatrix[1,0] - rotMatrix[2,0] * yp
+    e = rotMatrix[1,1] - rotMatrix[2,1] * yp
+    f = tVec[1,0] - tVec[2,0] * yp
     q = a*e-d*b
     
     X = (c*e - f*b)/q
     Y = (f*a - c*d)/q
     
-    return X,Y
+    shape = (corners.shape[0],1,3)
+    XYZ = np.array([X, Y, np.zeros(shape[0])])
+    XYZ = np.reshape(XYZ, shape)
+    
+    return XYZ

test.py
@@ -24,7 +24,7 @@ distCoeffsFile = "./resources/PTZchessboard/zoom 0.0/ptzDistCoeffs.npy"
 linearCoeffsFile = "./resources/PTZchessboard/zoom 0.0/ptzLinearCoeffs.npy"
 
 rvecOptimFile = "./resources/PTZchessboard/zoom 0.0/ptzSheetRvecOptim.npy"
-tvecOptimFile = "./resources/PTZchessboard/zoom 0.0/ptzSheetTvecOptim.npy"
+tVecOptimFile = "./resources/PTZchessboard/zoom 0.0/ptzSheettVecOptim.npy"
 
 # %% LOAD DATA
 img = cv2.imread(imageFile)
@@ -35,7 +35,8 @@ distCoeffs = np.load(distCoeffsFile)
 cameraMatrix = np.load(linearCoeffsFile)
 
 rVec = np.load(rvecOptimFile)
-tVec = np.load(tvecOptimFile)
+rotMatrix, _ = cv2.Rodrigues(rVec)
+tVec = np.load(tVecOptimFile)
 
 # %% test distortion
 r = np.linspace(0,10,100)
@@ -57,6 +58,45 @@ rd = distortRadius(r, distCoeffs)
 plt.plot(r,rd)
 
 # %%
-u,v = corners[0,0]
+u,v = corners[10,0]
 
-inverseRational(u, v, cameraMatrix, distCoeffs)
+XYZ = inverseRational(corners, cameraMatrix, distCoeffs, rotMatrix, tVec)
+
+# %% PLOT 3D SCENE
+
+[x,y,z] = rotMatrix # get from ortogonal matrix
+
+fig = plt.figure()
+from mpl_toolkits.mplot3d import Axes3D
+
+ax = fig.gca(projection='3d')
+
+ax.plot([0, tVec[0,0]],
+        [0, tVec[1,0]],
+        [0, tVec[2,0]])
+        
+ax.plot([tVec[0,0], tVec[0,0] + x[0]],
+        [tVec[1,0], tVec[1,0] + x[1]],
+        [tVec[2,0], tVec[2,0] + x[2]])
+
+ax.plot([tVec[0,0], tVec[0,0] + y[0]],
+        [tVec[1,0], tVec[1,0] + y[1]],
+        [tVec[2,0], tVec[2,0] + y[2]])
+
+ax.plot([tVec[0,0], tVec[0,0] + z[0]],
+        [tVec[1,0], tVec[1,0] + z[1]],
+        [tVec[2,0], tVec[2,0] + z[2]])
+
+# ax.scatter(XYZ[:,0,0], XYZ[:,0,1], XYZ[:,0,2]) # da muy mal!!!!
+
+ax.scatter(objectPoints[0,:,0],
+           objectPoints[0,:,1],
+           objectPoints[0,:,2])
+
+
+#ax.legend()
+#ax.set_xlim3d(0, 1)
+#ax.set_ylim3d(0, 1)
+#ax.set_zlim3d(0, 1)
+
+plt.show()