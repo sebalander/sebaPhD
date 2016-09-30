d71d464ee05d5c8143aff71ee01fe29ebf490022
sebalander <seba.arroyo7@gmail.com>
2016-09-30 17:40:07 -0300

    testing homography, problems with definitions of ref frames?

--- calibrateRegularCameraIntr.py
+++ calibrateRegularCameraIntr.py
@@ -32,7 +32,6 @@ imgShapeFile = "./resources/ptzImgShape.npy"
 corners = np.load(cornersFile).transpose((0,2,1,3))
 fiducialPoints = np.load(patternFile)
 imgSize = tuple(np.load(imgShapeFile))
-f = 1e3
 images = glob.glob(imagesFolder+'*.jpg')
 
 # output files
@@ -121,37 +120,6 @@ rVecsIni, tVecsIni = initialPoses1(fiducialPoints, corners, imgSize, f)
 
 # %% 
 
-def rotTras2(fiducialPoints, corner, imgSize, f):
-    
-    homography = cv2.findHomography(fiducialPoints[0,:,:2], (corner[:,0]-np.array(imgSize)/2)/f, method='RANSAC')[0]
-    
-    r1 = homography[0]
-    r2 = homography[1]
-    tVec = homography[2].reshape((3,1))
-    
-    r3 = np.cross(r1,r2)
-    rot = np.array([r1, r2, r3])
-    
-    rot = rot.dot(linalg.inv(linalg.sqrtm(rot.T.dot(rot))))
-    rVec, _ = cv2.Rodrigues(rot)
-    
-    return rVec, tVec
-
-
-
-def initialPoses2(fiducialPoints, corners, imgSize, f):
-    
-    initialPoses = [rotTras2(fiducialPoints, corner, imgSize, f) for corner in corners]
-    
-    rVecsIni = [pose[0] for pose in initialPoses]
-    tVecsIni = [pose[1] for pose in initialPoses]
-    
-    return rVecsIni, tVecsIni
-
-rVecsIni, tVecsIni = initialPoses2(fiducialPoints, corners, imgSize, f)
-
-# %% 
-
 def initialPoses3(fiducialPoints, corners, imgSize, f):
     
     t = np.mean(fiducialPoints[0],axis=0)+[0,0,1]
@@ -169,20 +137,93 @@ def initialPoses3(fiducialPoints, corners, imgSize, f):
 
 rVecsIni, tVecsIni = initialPoses3(fiducialPoints, corners, imgSize, f)
 
+# %% 
+
+def rotTras2(fiducialPoints, corner, imgSize, f):
+    # i = 8
+    # corner = corners[i]
+    srcPoints = fiducialPoints[0,:,:2]
+    dstPoints = (corner[:,0] - np.array(imgSize)/2) / f
+    method = 0 #cv2.RANSAC
+    
+    homography = cv2.findHomography(srcPoints, dstPoints, method=method)[0]
+    
+    r1 = homography[:,0]
+    r2 = homography[:,1]
+    r3 = np.cross(r1,r2)
+    
+    # get versors of A described in B ref frame
+    #r1B = homography[:,0]
+    #r2B = homography[:,1]
+    #r3B = np.cross(r1B,r2B)
+    
+    # convert to versors of B described in A ref frame
+    #r1 = np.array([r1B[0],r2B[0],r3B[0]])
+    #r2 = np.array([r1B[1],r2B[1],r3B[1]])
+    #r3 = np.array([r1B[2],r2B[2],r3B[2]])
+    
+    rot = np.array([r1, r2, r3]).T
+    # make sure is orthonormal
+    rot = rot.dot(linalg.inv(linalg.sqrtm(rot.T.dot(rot))))
+    rVec, _ = cv2.Rodrigues(rot) # make into rot vector
+    
+    # rotate to get displ redcribed in A
+    # tVec = np.dot(rot, -homography[2]).reshape((3,1))
+    
+    tVec = homography[:,2].reshape((3,1))
+    
+    return [rVec, tVec]
+
+
+
+def initialPoses2(fiducialPoints, corners, imgSize, f):
+    
+    initialPoses = [rotTras2(fiducialPoints, corner, imgSize, f) for corner in corners]
+    
+    rVecsIni = [pose[0] for pose in initialPoses]
+    tVecsIni = [pose[1] for pose in initialPoses]
+    
+    return rVecsIni, tVecsIni
+
+
+
 # %%
 
 model= 'rational'
-i = 8
+i = 3
 
 # %% 
 # hacer optimizacion para terminar de ajustar el pinhole y asi sacar la pose para cada imagen. usar las funciones de las librerias extrinsecas
 # es para cada imagen por separado
-
+f = 1e3
 linearCoeffs = np.array([[f, 0, imgSize[0]/2],
                          [0, f, imgSize[1]/2],
                          [0, 0, 1]], dtype='float32')
 
 distCoeffs = np.zeros((14,1)) # no hay distorsion
+
+# %% test initial calibration INVERSE
+
+rVecsIni, tVecsIni = initialPoses2(fiducialPoints, corners, imgSize, f)
+
+
+fiducialProjectedIni = pc.inverse(corners[i],
+                                  rVecsIni[i], tVecsIni[i],
+                                  linearCoeffs, distCoeffs, model)
+
+# plot
+# pc.fiducialComparison(fiducialPoints, fiducialProjectedIni)
+pc.fiducialComparison3D(rVecsIni[i], tVecsIni[i], fiducialPoints, fiducialProjectedIni, label1 = 'Fiducial points', label2 = 'Projected points')
+
+
+# %% optimise rVec, tVec
+rVecOpt, tVecOpt, optParams = pc.calibrateInverse(fiducialPoints, corners[i], rVecsIni[i], tVecsIni[i], linearCoeffs, distCoeffs,model)
+
+fiducialProjectedOpt = pc.inverse(corners[i], rVecOpt, tVecOpt, linearCoeffs, distCoeffs,model)
+# plot
+pc.fiducialComparison(fiducialPoints, fiducialProjectedOpt)
+pc.fiducialComparison3D(rVecOpt, tVecOpt, fiducialPoints, fiducialProjectedOpt, label1 = 'Fiducial points', label2 = 'Optimised points')
+
 
 # %% test initial calibration DIRECT
 
@@ -200,25 +241,6 @@ rVecOpt, tVecOpt, optParams = pc.calibrateDirect(fiducialPoints, corners[i], rVe
 # test mapping with optimised conditions
 cornersProjectedOpt = pc.direct(fiducialPoints, rVecOpt, tVecOpt, linearCoeffs, distCoeffs, model)
 pc.cornerComparison(img, corners[i], cornersProjectedOpt)
-
-
-# %% test initial calibration INVERSE
-fiducialProjectedIni = pc.inverse(corners[i],
-                                  rVecsIni[i], tVecsIni[i],
-                                  linearCoeffs, distCoeffs, model)
-
-# plot
-pc.fiducialComparison(fiducialPoints, fiducialProjectedIni)
-pc.fiducialComparison3D(rVecsIni[i], tVecsIni[i], fiducialPoints, fiducialProjectedIni, label1 = 'Fiducial points', label2 = 'Projected points')
-
-
-# %% optimise rVec, tVec
-rVecOpt, tVecOpt, optParams = pc.calibrateInverse(fiducialPoints, corners[i], rVecsIni[i], tVecsIni[i], linearCoeffs, distCoeffs,model)
-
-fiducialProjectedOpt = pc.inverse(corners[i], rVecOpt, tVecOpt, linearCoeffs, distCoeffs,model)
-# plot
-pc.fiducialComparison(fiducialPoints, fiducialProjectedOpt)
-pc.fiducialComparison3D(rVecOpt, tVecOpt, fiducialPoints, fiducialProjectedOpt, label1 = 'Fiducial points', label2 = 'Optimised points')
 
 
 # %%
