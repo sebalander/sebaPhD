f6f66cfc07831bfbe706897ceca585c163604cf1
sebalander <seba.arroyo7@gmail.com>
2016-10-03 22:31:48 -0300

    homography manipulñation succesfull

--- calibration/calibrateRegularCameraIntr.py
+++ calibration/calibrateRegularCameraIntr.py
@@ -14,6 +14,9 @@ http://docs.opencv.org/ref/master/d9/d0c/group__calib3d.html#ga3207604e4b1a1758a
 import cv2
 import numpy as np
 import glob
+import matplotlib.pyplot as plt
+from scipy import linalg
+import poseCalibration as pc
 
 # %% LOAD DATA
 #imagesFolder = "./resources/fishChessboardImg/"
@@ -26,10 +29,10 @@ cornersFile = "./resources/PTZchessboard/zoom 0.0/ptzCorners.npy"
 patternFile = "./resources/chessPattern.npy"
 imgShapeFile = "./resources/ptzImgShape.npy"
 
-imgpoints = np.load(cornersFile)
-chessboardModel = np.load(patternFile)
+corners = np.load(cornersFile).transpose((0,2,1,3))
+fiducialPoints = np.load(patternFile)
 imgSize = tuple(np.load(imgShapeFile))
-images = glob.glob(imagesFolder+'*.png')
+images = glob.glob(imagesFolder+'*.jpg')
 
 # output files
 distCoeffsFile = "./resources/PTZchessboard/zoom 0.0/ptzDistCoeffs.npy"
@@ -37,39 +40,212 @@ linearCoeffsFile = "./resources/PTZchessboard/zoom 0.0/ptzLinearCoeffs.npy"
 rvecsFile = "./resources/PTZchessboard/zoom 0.0/ptzRvecs.npy"
 tvecsFile = "./resources/PTZchessboard/zoom 0.0/ptzTvecs.npy"
 
+# %% LINEAR APPROACH TO CAMERA CALIBRATION
+n = corners.shape[0] # number of images
+m = corners.shape[1] # points per image
+
+#i = 3
+#j = 31
+#x = corners[i,0,j]
+#p = fiducialPoints[0,j]
+cer = np.zeros(4)
+
+
+def lineas(x,y,xp,yp):
+    
+    lin = [[x, 0, -x*xp, y, 0, -y*xp, 1, 0, -xp],
+     [0, x, -x*yp, 0, y, -y*yp, 0, 1, -yp]]
+    
+    return lin
+
+i = 0 # indice de imagen, hasta n
+j = 0 # indice de punto, hasta m
+
+xp = corners[i,j,0,0] - imgSize[0]/2
+yp = corners[i,j,0,1] - imgSize[1]/2
+x = fiducialPoints[0,j,0]
+y = fiducialPoints[0,j,1]
+
+lineas(x,y,xp,yp)
+
+def H(fiducialPoints, corner, imgSize, f):
+    m = fiducialPoints.shape[1]
+    h = [ lineas(fiducialPoints[0,j,0],
+                 fiducialPoints[0,j,1],
+                 (corner[j,0,0] - imgSize[0]/2)/f,
+                 (corner[j,0,1] - imgSize[1]/2)/f)
+         for j in range(m)]
+    
+    h = np.array(h)
+    h = np.reshape(h,(m*2,9))
+    return h
+
+H(fiducialPoints, corners[0], imgSize, f)
+
+def rotTras1(fiducialPoints, corner, imgSize, f):
+    
+    h = H(fiducialPoints, corner, imgSize, f)
+    
+    # uso svd
+    U, S, V = linalg.svd(h)
+    # elijo el menor
+    v = V[8]
+    # chequear que no este repetido?
+    r1 = v[0:3]
+    r2 = v[3:6]
+    tVec = v[6:9].reshape((3,1))
+    
+    # np.dot(r1, r2) # no dan perpendiculares
+    r3 = np.cross(r1,r2)
+    
+    # componer matriz de rotacion cruda
+    # ortonormalizarla
+    rot = np.array([r1, r2, r3])
+    
+    rot = rot.dot(linalg.inv(linalg.sqrtm(rot.T.dot(rot))))
+    
+    rVec, _ = cv2.Rodrigues(rot)
+    
+    return rVec, tVec
+
+def initialPoses1(fiducialPoints, corners, imgSize, f):
+    initialPoses = [rotTras1(fiducialPoints, corner, imgSize, f) for corner in corners]
+    
+    rVecsIni = [pose[0] for pose in initialPoses]
+    tVecsIni = [pose[1] for pose in initialPoses]
+    
+    return rVecsIni, tVecsIni
+
+rVecsIni, tVecsIni = initialPoses1(fiducialPoints, corners, imgSize, f)
+
+# %% 
+
+def initialPoses3(fiducialPoints, corners, imgSize, f):
+    
+    t = np.mean(fiducialPoints[0],axis=0)+[0,0,1]
+    tVec = t.reshape((3,1))
+    
+    rot = np.array([[1,0,0],
+                    [0,-1,0],
+                    [0,0,-1]], dtype='float32')
+    rVec = cv2.Rodrigues(rot)[0]
+    
+    rVecsIni = [rVec]*corners.shape[0]
+    tVecsIni = [tVec]*corners.shape[0]
+    
+    return rVecsIni, tVecsIni
+
+rVecsIni, tVecsIni = initialPoses3(fiducialPoints, corners, imgSize, f)
+
+# %% 
+
+def rotTras2(fiducialPoints, corner, imgSize, f):
+    # i = 1
+    # corner = corners[i]
+    srcPoints = fiducialPoints[0,:,:2]
+    dstPoints = (corner[:,0] - np.array(imgSize)/2) / f
+    method = 0 #cv2.RANSAC
+    
+    homography = cv2.findHomography(srcPoints, dstPoints, method=method)[0]
+    
+    # r1 = homography[:,0]
+    # r2 = homography[:,1]
+    # r3 = np.cross(r1,r2)
+    
+    # get versors of A described in B ref frame
+    r1B = homography[:,0]
+    r2B = homography[:,1]
+    r3B = np.cross(r1B,r2B)
+    
+    # convert to versors of B described in A ref frame
+    r1 = np.array([r1B[0],r2B[0],r3B[0]])
+    r2 = np.array([r1B[1],r2B[1],r3B[1]])
+    r3 = np.array([r1B[2],r2B[2],r3B[2]])
+    
+    rot = np.array([r1, r2, r3]).T
+    # make sure is orthonormal
+    rot = rot.dot(linalg.inv(linalg.sqrtm(rot.T.dot(rot))))
+    rVec, _ = cv2.Rodrigues(rot) # make into rot vector
+    
+    # rotate to get displ redcribed in A
+    # tVec = np.dot(rot, -homography[2]).reshape((3,1))
+    tVec = homography[:,2].reshape((3,1))
+    
+    return [rVec, tVec]
+
+
+
+def initialPoses2(fiducialPoints, corners, imgSize, f):
+    
+    initialPoses = [rotTras2(fiducialPoints, corner, imgSize, f) for corner in corners]
+    
+    rVecsIni = [pose[0] for pose in initialPoses]
+    tVecsIni = [pose[1] for pose in initialPoses]
+    
+    return [rVecsIni, tVecsIni]
+
+
+
 # %%
-n = len(imgpoints)  # cantidad de imagenes
-# Parametros de entrada/salida de la calibracion
-rvecs = ()
-tvecs = ()
-cameraMatrix = ()
-distCoeffs = ()
 
-# enable possible coefficients
-flags = cv2.CALIB_ZERO_TANGENT_DIST + cv2.CALIB_RATIONAL_MODEL
+model= 'rational'
 
-objpoints = [chessboardModel]*n
+# %% 
+# hacer optimizacion para terminar de ajustar el pinhole y asi sacar la pose para cada imagen. usar las funciones de las librerias extrinsecas
+# es para cada imagen por separado
+f = 3e1
+linearCoeffs = np.array([[f, 0, imgSize[0]/2],
+                         [0, f, imgSize[1]/2],
+                         [0, 0, 1]], dtype='float32')
 
-calibrationCriteria = (cv2.TERM_CRITERIA_EPS
-                       + cv2.TERM_CRITERIA_MAX_ITER,
-                       1000, # max number of iterations
-                       0.0001) # min accuracy
+distCoeffs = np.zeros((14,1)) # no hay distorsion
 
-# %% OPTIMIZAR 
-rms, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(objpoints,
-                                                                  imgpoints,
-                                                                  imgSize,
-                                                                  cameraMatrix,
-                                                                  distCoeffs,
-                                                                  rvecs,
-                                                                  tvecs,
-                                                                  flags)
+# %% test initial calibration INVERSE
 
-# (k1,k2,p1,p2[,k3[,k4,k5,k6[,s1,s2,s3,s4[,τx,τy]]]])
+rVecsIni, tVecsIni = initialPoses2(fiducialPoints, corners, imgSize, f)
 
 
-# %% SAVE PARAMETERS, INTRINSIC AND EXTRINSIC
-np.save(distCoeffsFile, distCoeffs)
-np.save(linearCoeffsFile, cameraMatrix)
-np.save(rvecsFile, rvecs)
-np.save(tvecsFile, tvecs)
+# %%
+i = 9
+
+
+fiducialProjectedIni = pc.inverse(corners[i],
+                                  rVecsIni[i], tVecsIni[i],
+                                  linearCoeffs, distCoeffs, model)
+
+# plot
+# pc.fiducialComparison(fiducialPoints, fiducialProjectedIni)
+pc.fiducialComparison3D(rVecsIni[i], tVecsIni[i], fiducialPoints, fiducialProjectedIni, label1 = 'Fiducial points', label2 = 'Projected points')
+
+
+# %% optimise rVec, tVec
+rVecOpt, tVecOpt, optParams = pc.calibrateInverse(fiducialPoints, corners[i], rVecsIni[i], tVecsIni[i], linearCoeffs, distCoeffs,model)
+
+fiducialProjectedOpt = pc.inverse(corners[i], rVecOpt, tVecOpt, linearCoeffs, distCoeffs,model)
+# plot
+pc.fiducialComparison(fiducialPoints, fiducialProjectedOpt)
+pc.fiducialComparison3D(rVecOpt, tVecOpt, fiducialPoints, fiducialProjectedOpt, label1 = 'Fiducial points', label2 = 'Optimised points')
+
+
+# %% test initial calibration DIRECT
+
+cornersProjectedIni = pc.direct(fiducialPoints,
+                                rVecsIni[i], tVecsIni[i],
+                                linearCoeffs, distCoeffs, model)
+
+# plot corners in image
+img = cv2.imread(images[i])
+pc.cornerComparison(img, corners[i], cornersProjectedIni)
+
+# %% optimise rVec, tVec
+rVecOpt, tVecOpt, optParams = pc.calibrateDirect(fiducialPoints, corners[i], rVecsIni[i], tVecsIni[i], linearCoeffs, distCoeffs, model)
+
+# test mapping with optimised conditions
+cornersProjectedOpt = pc.direct(fiducialPoints, rVecOpt, tVecOpt, linearCoeffs, distCoeffs, model)
+pc.cornerComparison(img, corners[i], cornersProjectedOpt)
+
+
+# %%
+# para todas las imagenes juntas dejar la pose fija y optimizar los parametros de distorsion 
+
+# %% optimizar todo junto a la vez tomando lo anterior como condiciones iniciales.
--- calibration/testHomography.py
+++ calibration/testHomography.py
@@ -36,39 +36,95 @@ linearCoeffsFile = "./resources/PTZchessboard/zoom 0.0/ptzLinearCoeffs.npy"
 rvecsFile = "./resources/PTZchessboard/zoom 0.0/ptzRvecs.npy"
 tvecsFile = "./resources/PTZchessboard/zoom 0.0/ptzTvecs.npy"
 
+
+
+# %%
+
+def pose2homogr(rVec,tVec,f):
+    
+    # calcular las puntas de los versores
+    if rVec.shape == (3,3):
+        [x,y,z] = rVec
+    else:
+        [x,y,z], _ = cv2.Rodrigues(rVec)
+    
+    
+    H = np.array([x,y,tVec[:,0]]).T
+    
+    return f*H
+
+
+
+def homogr2pose(H):
+    r1B = H[:,0]
+    r2B = H[:,1]
+    r3B = np.cross(r1B,r2B)
+    
+    # convert to versors of B described in A ref frame
+    r1 = np.array([r1B[0],r2B[0],r3B[0]])
+    r2 = np.array([r1B[1],r2B[1],r3B[1]])
+    r3 = np.array([r1B[2],r2B[2],r3B[2]])
+    
+    rot = np.array([r1, r2, r3]).T
+    # make sure is orthonormal
+    rotNorm = rot.dot(linalg.inv(linalg.sqrtm(rot.T.dot(rot))))
+    rVec, _ = cv2.Rodrigues(rotNorm) # make into rot vector
+    
+    # rotate to get displ redcribed in A
+    # tVec = np.dot(rot, -homography[2]).reshape((3,1))
+    tVec = H[:,2].reshape((3,1))
+    
+    # rescale
+    k = np.sqrt(np.linalg.norm(r1)*np.linalg.norm(r2))
+    tVec = tVec / k
+    
+    return [rVec, tVec]
+
+
+
 # %% custom homography
 
-#xA = np.array([1,4,6])
-#yA = np.array([-1,0,2])
+rVec = np.array([[1,0,0],[0,-1,0],[0,0,-1]])
+tVec = np.array([[4],[5],[8]])*2 +1
+f = 4.215687215
+
+H = pose2homogr(rVec, tVec, f)
+
+# %% randdom homography
+rVec = np.random.rand(3)*20 - 10
+tVec = np.random.rand(3).reshape(3,1)
+f = np.random.rand()
+
+H = pose2homogr(rVec, tVec, f)
+pc.fiducialComparison3D(rVec,tVec,fiducialPoints)
 
 
-Hcust = 2*np.array([[0,1,0],[0,0,1],[1,0,0]])
+# %%
 
 src = fiducialPoints[0]+[0,0,1]
-src2 = np.array([np.dot(Hcust, sr) for sr in src])
+src2 = np.array([np.dot(H, sr) for sr in src])
 dst = np.array([src2[:,0]/src2[:,2], src2[:,1]/src2[:,2]]).T
 
 # %% plot 
 
+plt.figure()
 plt.plot(src[:,0], src[:,1],'+k')
 plt.plot(dst[:,0], dst[:,1],'xr')
 
+
+
 # %% fit with function
 
-H = cv2.findHomography(src[:,:2], dst, method=8)[0]
+H2 = cv2.findHomography(src[:,:2], dst, method=8)[0]
 
-Hcust
 H
-
+H2
 # %%
 
-rot = np.array([[1,0,0],
-                [0,-1,0],
-                [0,0,-1]], dtype='float32')
-rVec = cv2.Rodrigues(rot)[0]
+rVec2, tVec2 = homogr2pose(H2)
 
-rot2 = cv2.Rodrigues(rVec)[0]
+tVec
+tVec2
+# %%
 
-rot
-rot2
-
+pc.fiducialComparison3D(rVec2,tVec2,fiducialPoints)
