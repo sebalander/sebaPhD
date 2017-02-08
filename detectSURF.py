b7f1027b1d7aaab78b3de0749cb60b13fb41077e
sebalander <seba.arroyo7@gmail.com>
2017-02-08 16:39:35 -0300

    packed inside object

--- detectSURF.py
+++ detectSURF.py
@@ -20,108 +20,144 @@ from skimage.color.adapt_rgb import adapt_rgb, each_channel
 def localEqualize(image, selem):
     return rank.equalize(image, selem=selem)
 
-# %% data de entrada
-path = '/home/sebalander/Code/VisionUNQextra/Videos y Mediciones/2016-11-13 medicion/'
-roiFile = path + 'roi.png'
-videosFile = path + 'Videos/vca_2016*.avi'
-vidOutputBSSURF = path + 'Videos/vca_BS_SURF.avi'
-
-showOnscreen = True
-writeToDIsk = True
-
-surfTrhes = 500
-surfHist = 5000
-blurSigmaX = 2
-equalDiskSize = 500
-nVideosMax = 5  # number of videos to process, default 0
+## %% data de entrada
+#path = '/home/sebalander/Code/VisionUNQextra/Videos y Mediciones/2016-11-13 medicion/'
+#roiFile = path + 'roi.png'
+#videosFile = path + 'Videos/vca_2016*.avi'
+#vidOutputBSSURF = path + 'Videos/vca_BS_SURF.avi'
+#
+#showOnscreen = True
+#writeToDIsk = True
+#
+#surfTrhes = 500
+#surfHist = 5000
+#blurSigmaX = 2
+#equalDiskSize = 500
+#nVideosMax = 5  # number of videos to process, default 0
 
 # %% preparacion o inicializacion
+class detector():
+    '''
+    para detectar BS y SURF
+    '''
+    def __init__(self, path,
+                 roiFile='roi.png',
+                 videosFile='Videos/vca_2016*.avi',
+                 vidOutputBSSURF='Videos/vca_BS_SURF.avi',
+                 surfThres=500,
+                 bsHist=5000,
+                 bsThres=16,
+                 blurSigmaX=2,
+                 equalDiskSize=500,
+                 nVideosMax=3,
+                 showOnscreen=True,
+                 writeToDIsk=True):
 
-vidList = glob(videosFile)
-vidList.sort()
-vidN = len(vidList)
+        self.showOnscreen = showOnscreen
+        self.writeToDIsk = writeToDIsk
 
-# filtering parameters
-blurKsize = (blurSigmaX*4+1, blurSigmaX*4+1)
-selem = disk(equalDiskSize)
+        # paths
+        self.path = path
+        self.roiFile = path + roiFile
+        self.videosFile = path + videosFile
+        self.vidOutputBSSURF = path + vidOutputBSSURF
 
-# open first video to get one frame sample
-vid = cv2.VideoCapture(vidList[0])
-vid.isOpened()
-frame = vid.read()[1]  # get one frame
-sh = np.shape(frame)  # get it's shape
-vid.release()
+        # generate list of videos
+        self.vidList = glob(self.videosFile)
+        self.vidList.sort()
+        self.vidN = len(self.vidList)
 
-roi = cv2.imread(roiFile, cv2.IMREAD_GRAYSCALE)[:904]  # recorto xq es mas chico?
-im = dc(frame)
+        # filtering parameters
+        self.blurSigmaX = blurSigmaX
+        self.blurKsize = (blurSigmaX*4+1, blurSigmaX*4+1)
+        self.selem = disk(equalDiskSize)
 
-# create surf object
-surf = cv2.xfeatures2d.SURF_create(surfTrhes)
-bs = cv2.createBackgroundSubtractorMOG2(history=surfHist,
-                                        varThreshold=16,
-                                        detectShadows=False)
+        # open first video to get one frame sample
+        vid = cv2.VideoCapture(self.vidList[0])
+        vid.isOpened()
+        frame = vid.read()[1]  # get one frame
+        self.sh = np.shape(frame)  # get it's shape
+        vid.release()
 
-# an image of plain color
-coloredIm = np.array([[[255, 255, 0] for j in range(sh[1])]
-                      for i in range(sh[0])], dtype=np.uint8)
+        # recorto xq es mas chico?
+        self.roi = cv2.imread(self.roiFile, cv2.IMREAD_GRAYSCALE)[:904]
+        self.im = dc(frame)
 
+        # create surf object
+        self.surf = cv2.xfeatures2d.SURF_create(surfThres)
+        self.bs = cv2.createBackgroundSubtractorMOG2(history=bsHist,
+                                                     varThreshold=bsThres,
+                                                     detectShadows=False)
 
-# %% ejecucion
+        # an image of plain color
+        self.coloredIm = np.array([[[255, 255, 0] for j in range(self.sh[1])]
+                              for i in range(self.sh[0])], dtype=np.uint8)
 
-# open ocv window
-if showOnscreen:
-    cv2.namedWindow('frame',
-                    cv2.WINDOW_KEEPRATIO+cv2.WINDOW_AUTOSIZE)
+    # %% ejecucion
+    def detectar(self):
+        # open ocv window
+        if self.showOnscreen:
+            cv2.namedWindow('frame',
+                            cv2.WINDOW_KEEPRATIO+cv2.WINDOW_AUTOSIZE)
 
-# open video for writing
-if writeToDIsk:
-    fcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
-    out = cv2.VideoWriter(vidOutputBSSURF, fcc, 10, sh[:2])
+        # open video for writing
+        if self.writeToDIsk:
+            fcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+            out = cv2.VideoWriter(self.vidOutputBSSURF,
+                                  fcc, 10, self.sh[:2])
 
-for i, vidFile in enumerate(vidList):
-    vid = cv2.VideoCapture(vidFile)
-    framesN = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+        for i, vidFile in enumerate(self.vidList):
+            vid = cv2.VideoCapture(vidFile)
+            framesN = vid.get(cv2.CAP_PROP_FRAME_COUNT)
 
-    while vid.get(cv2.CAP_PROP_POS_FRAMES) < framesN:
+            while vid.get(cv2.CAP_PROP_POS_FRAMES) < framesN:
+                frame = vid.read()[1]
 
-        frame = vid.read()[1]
+                # find surf keypoints
+                keypoints_now, descriptors_now = (
+                            self.surf.detectAndCompute(frame, mask=self.roi))
 
-        frame2 = cv2.GaussianBlur(frame, blurKsize, blurSigmaX)  # reduce noise
-        frame2 = localEqualize(frame2, selem)
-
-        fgmask = bs.apply(frame2) #  esta linea da error:
-                                  #  cv2.error: /build/opencv/src/opencv-3.1.0/modules/python/src2/cv2.cpp:163: error: (-215) The data should normally be NULL! in function allocate
-
-        im = cv2.bitwise_and(coloredIm, coloredIm, mask=fgmask)
-        im = cv2.addWeighted(frame, 0.5, im, 0.5, 0)
-
-        keypoints_now, descriptors_now = surf.detectAndCompute(frame, mask=roi)
-        im = cv2.drawKeypoints(im, keypoints_now, im)
-        textIm = "Frame %d/%d. " % (vid.get(cv2.CAP_PROP_POS_FRAMES), framesN)
-        textIm = textIm + "Video  " + vidFile[84:]
-
-        print(textIm)
-
-        im = cv2.putText(im, textIm, (50, 850),
-                         cv2.FONT_HERSHEY_COMPLEX, 1, [0, 0, 0])
-        if writeToDIsk:
-            out.write(im)
-
-        if showOnscreen:
-            cv2.imshow('frame', cv2.pyrDown(im))
-            keyPressed = cv2.waitKey(-1)
+                # aplly to BS algorith MOG2
+                frame2 = cv2.GaussianBlur(frame, self.blurKsize,
+                                          self.blurSigmaX)  # reduce noise
+                frame2 = localEqualize(frame2, self.selem)
+                fgmask = self.bs.apply(frame2)
+                # esta linea da error:
+                # cv2.error: /build/opencv/src/opencv-3.1.0/modules/python/src2/cv2.cpp:163: error: (-215) The data should normally be NULL! in function allocate
 
 
-    vid.release()
+                # compose images
+                im = cv2.bitwise_and(self.coloredIm, self.coloredIm,
+                                     mask=fgmask)
+                im = cv2.addWeighted(frame, 0.5, im, 0.5, 0)
+                im = cv2.drawKeypoints(im, keypoints_now, im)
 
-    # salir despues de hacer 5 videos
-    if nVideosMax!=0 and i > nVideosMax:
-        break
-
-# close
-if writeToDIsk:
-    out.release()
-
-if showOnscreen:
-    cv2.destroyAllWindows()
+                textIm = "Frame %d/%d. " % (vid.get(cv2.CAP_PROP_POS_FRAMES),
+                                            framesN)
+                textIm = textIm + "Video  " + vidFile[len(self.path):]
+        
+                print(textIm)
+        
+                im = cv2.putText(im, textIm, (50, 850),
+                                 cv2.FONT_HERSHEY_COMPLEX, 1, [0, 0, 0])
+                if self.writeToDIsk:
+                    out.write(im)
+        
+                if self.showOnscreen:
+                    cv2.imshow('frame', cv2.pyrDown(im))
+                    cv2.waitKey(1)
+        
+        
+            vid.release()
+        
+            # salir despues de hacer 5 videos
+            if self.nVideosMax!=0 and i > self.nVideosMax:
+                break
+        
+        # close
+        if self.writeToDIsk:
+            out.release()
+        
+        if self.showOnscreen:
+            cv2.destroyAllWindows()
 
--- testDetection.py
+++ testDetection.py
@@ -6,17 +6,41 @@ testear la deteccion surg y BS
 
 @author: sebalander
 """
-
+# %%
 import detectSURF
+import importlib
 
 # %% llamar con stos parametros de entrada
 # crear clase con esto
 path = '/home/sebalander/Code/VisionUNQextra/Videos y Mediciones/2016-11-13 medicion/'
-imFile = path + "vcaSnapShot.png"
-roiFile = path + 'roi.png'
-videosFile = path + 'Videos/vca_2016*.avi'
-vidOutputBSSURF = path + 'Videos/vca_BS_SURF.avi'
+roiFile='roi.png'
+videosFile='Videos/vca_2016*.avi'
+vidOutputBSSURF='Videos/vca_BS_SURF.avi'
+surfThres=500
+bsHist=5000
+bsThres=16
+blurSigmaX=2
+equalDiskSize=500
+nVideosMax=3
+showOnscreen = True
+writeToDIsk = True
 
 # %% preparar para leer videos
+importlib.reload(detectSURF)
+dtc = detectSURF.detector(path,
+                          roiFile,
+                          videosFile,
+                          vidOutputBSSURF,
+                          surfThres,
+                          bsHist,
+                          bsThres,
+                          blurSigmaX,
+                          equalDiskSize,
+                          nVideosMax,
+                          showOnscreen,
+                          writeToDIsk)
+# vars(dtc)
 
+# %%
+dtc.detectar()
 
