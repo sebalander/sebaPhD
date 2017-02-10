0a4a36e799969e3e2ccb85eee77653735c8c4406
sebalander <seba.arroyo7@gmail.com>
2017-02-10 20:59:09 -0300

    matching features dirty use lowes ratio or smthg

--- detect/testDetection.py
+++ detect/testDetection.py
@@ -7,38 +7,30 @@ testear la deteccion surg y BS
 @author: sebalander
 """
 # %%
-import detectSURF
-import importlib
+import trackFP
+from importlib import reload as rld
 
 # %% llamar con stos parametros de entrada
 # crear clase con esto
-path = '/home/sebalander/Code/VisionUNQextra/Videos y Mediciones/2016-11-13 medicion/'
-roiFile='roi.png'
-videosFile='Videos/vca_2016*.avi'
-vidOutputBSSURF='Videos/vca_BS_SURF.avi'
-surfThres=500
-bsHist=5000
-bsThres=16
-blurSigmaX=2
-equalDiskSize=500
-nVideosMax=3
+path = '/home/sebalander/Code/VisionUNQextra/Videos y Mediciones/'
+path = path + '2016-11-13 medicion/'
+roiFile = 'roi.png'
+videosFile = 'Videos/vca_2016*.avi'
+vidOutputBSSURF = 'Videos/vca_BS_SURF.avi'
+surfThres = 500
+bsHist = 5000
+bsThres = 16
+blurSigmaX = 2
+equalDiskSize = 500
+nVideosMax = 3
 showOnscreen = True
 writeToDIsk = True
 
 # %% preparar para leer videos
-importlib.reload(detectSURF)
-dtc = detectSURF.detector(path,
-                          roiFile,
-                          videosFile,
-                          vidOutputBSSURF,
-                          surfThres,
-                          bsHist,
-                          bsThres,
-                          blurSigmaX,
-                          equalDiskSize,
-                          nVideosMax,
-                          showOnscreen,
-                          writeToDIsk)
+rld(trackFP)
+dtc = trackFP.detector(path, roiFile, videosFile, vidOutputBSSURF,
+                       surfThres, bsHist, bsThres, blurSigmaX, equalDiskSize,
+                       nVideosMax, showOnscreen, writeToDIsk)
 # vars(dtc)
 
 # %%
