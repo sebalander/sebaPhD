5f1bc9582ea20a072c6829d04dc1732ed22ca388
sebalander <sarroyo@gmx.com>
2017-04-13 21:55:39 -0300

    plotted error dependence on pose

--- dev/calibExternNov2016.py
+++ dev/calibExternNov2016.py
@@ -15,6 +15,9 @@ from copy import deepcopy as dc
 from calibration import calibrator as cl
 from calibration import RationalCalibration as rational
 
+import corner
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from importlib import reload
@@ -91,40 +94,109 @@ def Esq(imagePoints, objectPoints, rVec, tVec, cameraMatrix, distCoeffs, model):
     return np.sum(e**2)
 
 
-# %% plotear la superficie a optimizar
+# %%
+# Calculates Rotation Matrix given euler angles.
+def eulerAnglesToRotationMatrix(theta):
+    '''
+    https://www.learnopencv.com/rotation-matrix-to-euler-angles/
+    '''
+     
+    R_x = np.array([[1,         0,                  0                   ],
+                    [0,         np.cos(theta[0]), -np.sin(theta[0]) ],
+                    [0,         np.sin(theta[0]), np.cos(theta[0])  ]
+                    ])
+         
+         
+                     
+    R_y = np.array([[np.cos(theta[1]),    0,      np.sin(theta[1])  ],
+                    [0,                     1,      0                   ],
+                    [-np.sin(theta[1]),   0,      np.cos(theta[1])  ]
+                    ])
+                 
+    R_z = np.array([[np.cos(theta[2]),    -np.sin(theta[2]),    0],
+                    [np.sin(theta[2]),    np.cos(theta[2]),     0],
+                    [0,                     0,                      1]
+                    ])
+                     
+                     
+    R = np.dot(R_z, np.dot( R_y, R_x ))
+ 
+    return R
+
+
+
+def rot2euler(R):
+    '''
+    takes rodrigues vector or rotation matrix and returns euler angles
+    https://www.learnopencv.com/rotation-matrix-to-euler-angles/
+    '''
+    if np.prod(R.shape)==3:
+        R = cv2.Rodrigues(R)[0]
+    
+    sy = np.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+     
+    singular = sy < 1e-6
+ 
+    if  not singular :
+        x = np.arctan2(R[2,1] , R[2,2])
+        y = np.arctan2(-R[2,0], sy)
+        z = np.arctan2(R[1,0], R[0,0])
+    else :
+        x = np.arctan2(-R[1,2], R[1,1])
+        y = np.arctan2(-R[2,0], sy)
+        z = 0
+ 
+    return np.array([x, y, z])
+
+# %% pl4otear la superficie a optimizar
 rVec = dc(rVecIni).reshape(-1)
 tVec = dc(tVecIni).reshape(-1)
 
-# ancho de intervalos donde me interesa generar datos
-a = np.pi / 20
-h = tVecIni[2] / 50
+# conviertto a coordenadas del mapa donde es más facil definir intervalos
+rM = rot2euler(-rVec)
+tM = - np.dot(cv2.Rodrigues(rVec)[0].T, tVec)
 
-N = 10**4  # cantidad de puntos
-rNoise = (np.random.rand(N,3) - 0.5) * a  # ruidos
-tNoise = (np.random.rand(N,3) - 0.5) * h
-RVecs = rVec + rNoise  # coordenadas
-TVecs = tVec + tNoise
+
+# ancho de intervalos donde me interesa generar datos
+a = np.pi / 10
+h = tM[2] / 5
+
+N = 10**5  # cantidad de puntos
+rNoise = (np.random.rand(N, 3) - 0.5) * a  # ruidos
+tNoise = (np.random.rand(N, 3) - 0.5) * h
+RVecsM = rM + rNoise  # coordenadas respecto al mapa, angulos de euler
+TVecsM = tM + tNoise
 EE = np.empty(N)
 
+# coordenadas de la camara
+RVecsC = np.empty_like(RVecsM)
+TVecsC = np.empty_like(TVecsM)
 
 # calculo errores
 for i in range(N):
-    EE[i] = Esq(imagePoints, objectPoints, RVecs[i], TVecs[i], cameraMatrix, distCoeffs, model)
-
-# all dat toghether
-samples = np.concatenate((RVecs, TVecs, EE.reshape((-1,1))),axis=1)
-
-import corner
-
-# figure = corner.corner(samples, labels=['r1', 'r2', 'r3', 't1', 't2', 't3', 'E'])
-
-we = np.exp(-samples[:,-1])
-#we /= np.sum(we)
-
-figure = corner.corner(samples[:,:-1], weights=we,
-                       labels=['r1', 'r2', 'r3', 't1', 't2', 't3'])
+    # convierto a coordenadas de la camara
+    r = eulerAnglesToRotationMatrix(RVecsM[i]).T
+    t = - np.dot(r, TVecsM[i])
+    EE[i] = Esq(imagePoints, objectPoints, r, t, cameraMatrix, distCoeffs, model)
 
 
+
+# all data toghether
+samples = np.concatenate((RVecsM, TVecsM),axis=1)
+labls = ['r1', 'r2', 'r3', 't1', 't2', 't3']
+
+#figure = corner.corner(samples, labels=labls)
+we = 1/EE # np.exp(-EE / np.mean(EE))
+
+figure = corner.corner(samples, weights=we, labels=labls)
+#
+#samples2 = np.concatenate((samples,EE.reshape((-1,1))),axis=1)
+#labls2 = labls.append('E')
+#figure = corner.corner(samples2, labels=labls2)
+#
+#for i in range(6):
+#    plt.figure()
+#    plt.scatter(samples[:,i],EE)
 
 # %% 
 def gradE2(imagePoints, objectPoints, rVec, tVec, cameraMatrix, distCoeffs, model):
