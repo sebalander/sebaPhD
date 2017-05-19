69c34b9ad38a750a62f78f32033b35d34203b106
sebalander <seba.arroyo7@gmail.com>
2017-05-19 20:29:49 -0300

    first covariance propagation appears succesfull

---
+++ dev/ellipseProjection.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 18:02:20 2017
+
+@author: sebalander
+"""
+
+# %%
+import sympy as sy
+import numpy as np
+
+# %% defino variables
+x, y, z, xm, ym, mx, my, s, a, b, c = sy.symbols('x, y, z, xm, ym, mx, my, s, a, b, c')
+r11, r12, r21, r22, r31, r32, tx, ty, tz = sy.symbols('r11, r12, r21, r22, r31, r32, tx, ty, tz')
+
+
+
+# %% expresion de la rototraslacion
+R = np.array([[r11, r12],[r21, r22], [r31, r32]])
+T = np.array([tx, ty, tz])
+xym = np.array([xm, ym])
+
+# coords en acamara ref frame from map coords
+x, y, z = R.dot(xym) + T
+
+
+# %% expresion del conoide
+C = np.array([[a,b],[b,c]])
+X3d = np.array([[x-mx*z],[y-my*z]])
+
+# esta expresion debe ser cero
+conoide = (X3d.T.dot(C).dot(X3d) - s*z).reshape(-1)[0]
+conexp = sy.expand(conoide)
+
+sy.collect(conexp, xm**2)
+sy.collect(conexp, ym**2)
+sy.collect(conexp, ym)
+sy.collect(conexp, xm*ym)
+
+cuadXmYm = conexp.as_poly(xm, ym)
+
+
+# %% tieneq que dar igual a esta expresion:
+X, Y, MX, MY, A, B, C = sy.symbols('X, Y, MX, MY, A, B, C')
+
+XX = np.array([[X - MX],[Y - MY]])
+equiv = XX.T.dot(np.array([[A,B],[B,C]])).dot(XX).reshape(-1)[0]
+equiv = sy.expand(equiv)
+equiv = equiv.as_poly(X,Y)
+
+# los tres elementos de la martiz de covarianza:
+A*X**2
++ 2*B*X*Y
++ C*Y**2
+# ahora el centro, dos ecs con 2 incognitas
++ (-2*A*MX - 2*B*MY)*X
++ (-2*B*MX - 2*C*MY)*Y
+# y esto parece que no sirve
++ A*MX**2
++ 2*B*MX*MY
++ C*MY**2
+
+# %%
+# de aca sale C11
+(a*mx**2*r31**2 - 2*a*mx*r11*r31 + a*r11**2 + 2*b*mx*my*r31**2 - 2*b*mx*r21*r31 - 2*b*my*r11*r31 + 2*b*r11*r21 + c*my**2*r31**2 - 2*c*my*r21*r31 + c*r21**2)*xm**2
+
+# de aca sale C12, C21
++ (2*a*mx**2*r31*r32 - 2*a*mx*r11*r32 - 2*a*mx*r12*r31 + 2*a*r11*r12 + 4*b*mx*my*r31*r32 - 2*b*mx*r21*r32 - 2*b*mx*r22*r31 - 2*b*my*r11*r32 - 2*b*my*r12*r31 + 2*b*r11*r22 + 2*b*r12*r21 + 2*c*my**2*r31*r32 - 2*c*my*r21*r32 - 2*c*my*r22*r31 + 2*c*r21*r22)*xm*ym
+
+# de aca sale C22
++ (a*mx**2*r32**2 - 2*a*mx*r12*r32 + a*r12**2 + 2*b*mx*my*r32**2 - 2*b*mx*r22*r32 - 2*b*my*r12*r32 + 2*b*r12*r22 + c*my**2*r32**2 - 2*c*my*r22*r32 + c*r22**2)*ym**2
+
+# de estas dos que siguen saco mux, muy
++ (2*a*mx**2*r31*tz - 2*a*mx*r11*tz - 2*a*mx*r31*tx + 2*a*r11*tx + 4*b*mx*my*r31*tz - 2*b*mx*r21*tz - 2*b*mx*r31*ty - 2*b*my*r11*tz - 2*b*my*r31*tx + 2*b*r11*ty + 2*b*r21*tx + 2*c*my**2*r31*tz - 2*c*my*r21*tz - 2*c*my*r31*ty + 2*c*r21*ty - r31*s)*xm
+
++ (2*a*mx**2*r32*tz - 2*a*mx*r12*tz - 2*a*mx*r32*tx + 2*a*r12*tx + 4*b*mx*my*r32*tz - 2*b*mx*r22*tz - 2*b*mx*r32*ty - 2*b*my*r12*tz - 2*b*my*r32*tx + 2*b*r12*ty + 2*b*r22*tx + 2*c*my**2*r32*tz - 2*c*my*r22*tz - 2*c*my*r32*ty + 2*c*r22*ty - r32*s)*ym
+
+# esta es redundante
++ a*mx**2*tz**2 - 2*a*mx*tx*tz + a*tx**2 + 2*b*mx*my*tz**2 - 2*b*mx*ty*tz - 2*b*my*tx*tz + 2*b*tx*ty + c*my**2*tz**2 - 2*c*my*ty*tz + c*ty**2 - s*tz
+
+
+
+
+
+
+
--- dev/uncertProp.py
+++ dev/uncertProp.py
@@ -12,7 +12,7 @@ from calibration import calibrator as cl
 import matplotlib.pyplot as plt
 from importlib import reload
 import scipy.linalg as ln
-
+import cv2
 
 # %%
 from scipy.special import chdtriv
@@ -57,7 +57,7 @@ chessboardModel = np.load(patternFile)
 imgSize = tuple(np.load(imgShapeFile))
 images = glob.glob(imagesFolder+'*.png')
 
-n = len(imagePoints)  # cantidad de imagenes
+nIm, _, nPts, _ = imagePoints.shape  # cantidad de imagenes
 # Parametros de entrada/salida de la calibracion
 objpoints = np.array([chessboardModel]*n)
 
@@ -73,13 +73,110 @@ rVecs = np.load(rVecsFile)
 tVecs = np.load(tVecsFile)
 
 # %% choose image
-j = 18
+j = 10 # 18
 print('\t imagen', j)
+imagePoints = np.load(cornersFile)
+imagePoints = imagePoints[j,0]
 
-# %% testeo cada paso de propagacion
-imagePoints[j,0].shape
+img = plt.imread(images[j])
+
 # covariances
-Cccd = 2 * np.array([np.eye(2)]*imagePoints[j,0].shape[0])
+Cccd = 2 * np.array([np.eye(2)]*imagePoints.shape[0])
+
+# %% plot initial uncertanties
+fig = plt.figure()
+ax = fig.gca()
+ax.imshow(img)
+for i in range(nPts):
+    x, y = imagePoints[i]
+    plotEllipse(ax, Cccd[i], x, y, 'b')
+
+#
+
+# %% propagate to homogemous
+
+xpp, ypp, Cpp = cl.ccd2hom(imagePoints, cameraMatrix, cov=Cccd)
+
+fig = plt.figure()
+ax = fig.gca()
+for i in range(nPts):
+    plotEllipse(ax, Cpp[i], xpp[i], ypp[i], 'b')
+
+# 
+
+# %% go to undostorted homogenous
+rpp = ln.norm([xpp, ypp], axis=0)
+
+# calculate ratio of undistorition and it's derivative wrt radius
+q, _, dqI = cl.undistort[model](rpp, distCoeffs, quot=True, der=True)
+
+xp = q * xpp # undistort in homogenous coords
+yp = q * ypp
+
+rp = rpp * q
+plt.plot(rp, rpp, '+')
+
+xpp2 = xpp**2
+ypp2 = ypp**2
+xypp = xpp * ypp
+dqIrpp = dqI / rpp
+
+# calculo jacobiano
+J = np.array([[xpp2, xypp],[xypp, ypp2]])
+J *= dqIrpp.reshape(1,1,-1)
+J[0,0,:] += q
+J[1,1,:] += q
+
+Cp = np.empty_like(Cpp)
+for i in range(nPts):
+    Cp[i] = J[:,:,i].dot(Cpp[i]).dot(J[:,:,i].T)
+
+
+fig = plt.figure()
+ax = fig.gca()
+for i in range(nPts):
+    plotEllipse(ax, Cp[i], xp[i], yp[i], 'b')
+
+# cargo la rototraslacion
+rV = rVecs[j]
+tV = tVecs[j]
+
+def rototrasCovariance(Cp, xp, yp, rV, tV):
+    a, b, _, c = Cp.flatten()
+    mx, my = (xp, yp)
+    r11, r12, r21, r22, r31,r32 = cv2.Rodrigues(rV)[0][:,:2].flatten()
+    tx, ty, tz = tV.flatten()
+    
+    C11 = (a*mx**2*r31**2 - 2*a*mx*r11*r31 + a*r11**2 + 2*b*mx*my*r31**2 - 2*b*mx*r21*r31 - 2*b*my*r11*r31 + 2*b*r11*r21 + c*my**2*r31**2 - 2*c*my*r21*r31 + c*r21**2)
+    
+    C12 = (a*mx**2*r31*r32 - a*mx*r11*r32 - a*mx*r12*r31 + a*r11*r12 + 2*b*mx*my*r31*r32 - b*mx*r21*r32 - b*mx*r22*r31 - b*my*r11*r32 - b*my*r12*r31 + b*r11*r22 + b*r12*r21 + c*my**2*r31*r32 - c*my*r21*r32 - c*my*r22*r31 + c*r21*r22)
+    
+    C22 = (a*mx**2*r32**2 - 2*a*mx*r12*r32 + a*r12**2 + 2*b*mx*my*r32**2 - 2*b*mx*r22*r32 - 2*b*my*r12*r32 + 2*b*r12*r22 + c*my**2*r32**2 - 2*c*my*r22*r32 + c*r22**2)
+    
+    C = np.array([[C11, C12], [C12, C22]])
+    
+    #s=1
+    #alfa = -(a*mx**2*r31*tz - a*mx*r11*tz - a*mx*r31*tx + a*r11*tx + 2*b*mx*my*r31*tz - b*mx*r21*tz - b*mx*r31*ty - b*my*r11*tz - b*my*r31*tx + b*r11*ty + b*r21*tx + c*my**2*r31*tz - c*my*r21*tz - c*my*r31*ty + c*r21*ty - r31*s)
+    #
+    #beta = -(a*mx**2*r32*tz - a*mx*r12*tz - a*mx*r32*tx + a*r12*tx + 2*b*mx*my*r32*tz - b*mx*r22*tz - b*mx*r32*ty - b*my*r12*tz - b*my*r32*tx + b*r12*ty + b*r22*tx + c*my**2*r32*tz - c*my*r22*tz - c*my*r32*ty + c*r22*ty - r32*s)
+    #
+    #MUx, MUy = ln.inv(C).dot([alfa, beta])
+
+    return C
+
+C = np.empty_like(Cp)
+for i in range(nPts):
+    C[i] = rototrasCovariance(Cp[i], xp[i], yp[i], rV, tV)
+
+xm, ym, _ = cl.inverse(imagePoints, rV, tV, cameraMatrix, distCoeffs, model).T
+
+fig = plt.figure()
+ax = fig.gca()
+ax.plot(xm, ym, '+', markersize=2)
+for i in range(nPts):
+    plotEllipse(ax, C[i], xm[i], ym[i], 'b')
+
+#
 
 # %% step by step calculation
 reload(cl)
@@ -96,18 +193,18 @@ ax = fig.gca()
 
 for i in range(1,len(xp)):
     print(Cp[i], xp[i], yp[i])
-    #plotEllipse(ax, Cp[i], xp[i], yp[i], 'k')
+    plotEllipse(ax, Cp[i], xp[i], yp[i], 'k')
     
-    c, mux, muy, col = (Cp[i], xp[i], yp[i], 'k')
-    
-    l, v  = ln.eig(c)
-    print(i, l)
-    
-    D = v.T*s # queda con los autovectores como filas
-    
-    xeli, yeli = np.dot(ln.inv(D), Xcirc)
-    
-    ax.plot(xeli+mux, yeli+muy, c=col, lw=0.5)
+#    c, mux, muy, col = (Cp[i], xp[i], yp[i], 'k')
+#    
+#    l, v  = ln.eig(c)
+#    print(i, l)
+#    
+#    D = v.T*np.sqrt(l) # queda con los autovectores como filas
+#    
+#    xeli, yeli = np.dot(ln.inv(D), Xcirc)
+#    
+#    ax.plot(xeli+mux, yeli+muy, c=col, lw=0.5)
 
 #
 
