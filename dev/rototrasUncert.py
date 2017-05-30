8d987587e583f64a396f485d98e815f1d25a637f
sebalander <seba.arroyo7@gmail.com>
2017-05-30 17:03:07 -0300

    some more jacobians

--- dev/rototrasUncert.py
+++ dev/rototrasUncert.py
@@ -18,40 +18,41 @@ r, a, b, tx, ty, tz = sy.symbols('r, a, b, tx, ty, tz')
 # vector a rotar
 vx, vy, vz = sy.symbols('vx, vy, vz')
 
-rV = sy.Matrix([r, a, b])
+rVsph = sy.Matrix([r, a, b])
 v = sy.Matrix([vx, vy, vz])
 tV = sy.Matrix([tx, ty, tz])
 
 ct = sy.cos(r)
+st = sy.sin(r)
 
 # %% esfericas y cartesianas
-rx = rV[0] * sy.cos(rV[1]) * sy.sin(rV[2])
-ry = rV[0] * sy.sin(rV[1]) * sy.sin(rV[2])
-rz = rV[0] * sy.cos(rV[2])
+rx = rVsph[0] * sy.cos(rVsph[1]) * sy.sin(rVsph[2])
+ry = rVsph[0] * sy.sin(rVsph[1]) * sy.sin(rVsph[2])
+rz = rVsph[0] * sy.cos(rVsph[2])
 
-rVcar = sy.Matrix([rx, ry, rz])
-rhat = rVcar / r
+rV = sy.Matrix([rx, ry, rz])
+rhat = rV / r
 
 # jacobiano de cartesianas derivadas respecto a esfericas
-JrVcar_rV = rVcar.jacobian(rV)
+JrV_rVsph = rV.jacobian(rVsph)
 
 # rotacion rodrigues
 v2 = ct * v + sy.sin(r) * rhat.cross(v) + rhat * (rhat.dot(v)) * (1 - ct)
 
-Jv2_rV = v2.jacobian(rV)
+Jv2_rVsph = v2.jacobian(rVsph)
 
 # %% pruebo con valores especificos
 valuesDict = {r:4, a:8, b:0.2, vx:1, vy:2, vz:3, tx:7, ty:8, tz:9}
 
-JrVcar_rV_eval = JrVcar_rV.subs(valuesDict).evalf()
-Jv2_rV_eval = Jv2_rV.subs(valuesDict).evalf()
+JrV_rVsph_eval = JrV_rVsph.subs(valuesDict).evalf()
+Jv2_rVsph_eval = Jv2_rVsph.subs(valuesDict).evalf()
 
-JrVcar_rV_eval = np.array(sy.matrix2numpy(JrVcar_rV_eval), dtype=float)
-Jv2_rV_eval = np.array(sy.matrix2numpy(Jv2_rV_eval), dtype=float)
+JrV_rVsph_eval = np.array(sy.matrix2numpy(JrV_rVsph_eval), dtype=float)
+Jv2_rVsph_eval = np.array(sy.matrix2numpy(Jv2_rVsph_eval), dtype=float)
 
 # busco el jacobiano del vector rotado con respecto al vector de rodrigues
 # en cartesianas
-Jv2_rVcar = Jv2_rV_eval.dot(np.linalg.inv(JrVcar_rV_eval))
+Jv2_rVcar = Jv2_rVsph_eval.dot(np.linalg.inv(JrV_rVsph_eval))
 
 # %% comparar con funcion donde se implementa
 def jacobianCartWrtSpher(r, a, b):
@@ -74,11 +75,11 @@ def jacobianCartWrtSpher(r, a, b):
     return J
 
 jacobianCartWrtSpher(4, 8, 0.2)
-JrVcar_rV_eval
+JrV_rVsph_eval
 
 # %%
 
-def jacobianRotatedWrtRvecSph(r,a,b,vx,vy,vz):
+def jacobianRotatedWrtRvecSph(r, a, b, vx, vy, vz):
     '''
     jacobian of the rotated vector with respect to the rotation vector
     expressed in its spherical coordinates
@@ -116,7 +117,33 @@ def jacobianRotatedWrtRvecSph(r,a,b,vx,vy,vz):
     return J
 
 #
-jacobianRotatedWrtRvecSph(4, 8, 0.2,1,2,3)
-Jv2_rV_eval
+jacobianRotatedWrtRvecSph(4, 8, 0.2, 1, 2, 3)
+Jv2_rVsph_eval
 
 
+# %% matriz de rotacion a partir del vector de rodriguez
+rrMat = sy.Matrix([[rx**2, rx*ry, rx*rz],[ry*rx, ry**2, ry*rz],[rz*rz, rz*ry, rz**2]]) / r**2
+rMat = sy.Matrix([[0, -rz, ry],[rz, 0, -rx],[-ry, rx, 0]]) / r
+
+R = sy.eye(3) * ct + (1-ct) * rrMat + st *rMat
+
+# %%
+xp, yp = sy.symbols('xp, yp')
+
+a = R[0,0] - R[2,0] * xp
+b = R[0,1] - R[2,1] * xp
+c = tx - tz * xp
+d = R[1,0] - R[2,0] * yp
+e = R[1,1] - R[2,1] * yp
+f = ty - tz * yp
+q = a * e - d * b
+
+x = (f * b - c * e) / q
+y = (c * d - f * a) / q
+
+X = sy.Matrix([x,y])
+Xp = sy.Matrix([xp,yp])
+
+# tengo que saacar el jacobiano respecto a xp, yp, rV y tV
+# para el jacobiano respecto a rV primero lo hago respecto a rVsph y uso
+# cain rule
