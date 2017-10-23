3bbb75c8f1a4b686c450145ed923ada316a8e3bd
sebalander <sarroyo@gmx.com>
2017-10-23 18:07:42 -0300

    lla 2 ecef in separate lib

---
+++ calibration/lla2eceflib.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 18:00:37 2017
+
+
+
+@author: sebalander
+"""
+# %% imports
+from numpy import deg2rad, cos, sin, sqrt, array, pi
+from numpy.linalg import norm
+
+# %% funciton definitions
+
+# constantes
+a = 6378137 # este
+f = 1 / 298.257223563
+#b = a * (1 - f)
+e2 = f * (2 - f)
+#e = np.sqrt(e2)
+#ep = e * a / b
+a2 = a**2
+b2 = a2 * (1 - f)**2
+
+
+def lla2ecef(lla):
+    '''
+    convierte lat lon y elevacion sobre WGS a coords ECEF
+    
+    N/rad ardius of curvatiure meters
+    phi es latitude 
+    lambda es longitud
+    h heigth above ellipsoid (meters)
+    
+    fuente:
+        Datum Transformations of GPS Positions, Application Note, 5th July 1999
+        https://microem.ru/files/2012/08/GPS.G1-X-00006.pdf
+    '''
+    llaT = lla.T # easier to manipulate vectors
+    
+    la = deg2rad(llaT[0]) # to radians
+    lo = deg2rad(llaT[1])
+    
+    cla = cos(la) # precalculate sine cosine
+    sla = sin(la)
+    clo = cos(lo)
+    slo = sin(lo)
+    
+    rad = a / sqrt(1 - e2 * sla**2) # radius of curvature
+    
+    r = rad + llaT[2] # add topoligical elevation wrt to ellipsoid
+    
+    X = r * cla * clo
+    Y = r * cla * slo
+    Z = (b2 * rad / a2 + llaT[2]) * sla
+    
+    return array([X, Y, Z]).T
+
+
+def ltpR(lla0):
+    '''
+    returns the ORIGIN and ROTATION matrix to postmultiply ecef coordinates
+    so thatraise RuntimeError("Cannot activate multiple GUI eventloops")
+    they are in the local tangent plane, east being x, north being y, height z.
+
+    given a X ecef 3D coord use
+    > (X - T).dot(R)
+    to convert to ltp frame of reference
+    '''
+    T = lla2ecef(lla0)
+    
+    fi = pi / 2 - deg2rad(lla0[0]) # decliancion desde el eje z
+    theta = deg2rad(lla0[1]) # longitud desde el eje x horizontal
+    
+    ct = cos(theta)
+    st = sin(theta)
+    cf = cos(fi)
+    sf = sin(fi)
+    
+    '''
+    defino los versores este oeste y altura
+    http://mathworld.wolfram.com/SphericalCoordinates.html
+    tambien coincide en como se convierte velocidades en 
+    Datum Transformations of GPS Positions, Application Note, 5th July 1999
+    '''
+    # versor en la direccion radial
+    rVersor = T / norm(T)
+    # e direccion este, versor theta
+    eastVersor = array([-st, ct, 0])
+    # versor norte, opueto el versor fi
+    northVersor = - array([ct * cf, st * cf, -sf])
+    
+    # proyecto sobre la rotacion que interesa
+    # matriz de rotacion para post multiplicar
+    R = array([eastVersor, northVersor, rVersor]).T
+    return R, T
+
+
+def lla2ltp(lla, lla0):
+    '''
+    converts lla to local tangent plane coords centering in another given lla
+    X is a (N,3) or (3,) vector containing lat, lon, alt in columns.
+    X0 is a (3,) vector. altitude is wrt WGS84 reference ellipsoid.
+    '''
+    
+    X = lla2ecef(lla)
+    
+    R, T = ltpR(lla0)
+    
+    return (X - T).dot(R)
--- dev/extrinsicCalibBayes.py
+++ dev/extrinsicCalibBayes.py
@@ -10,15 +10,16 @@ Created on Thu Sep  7 01:12:17 2017
 # %%
 #import glob
 import numpy as np
-import scipy.linalg as ln
+#import scipy.linalg as ln
 import matplotlib.pyplot as plt
-from importlib import reload
-from copy import deepcopy as dc
-import pyproj as pj
+#from importlib import reload
+#from copy import deepcopy as dc
+#import pyproj as pj
 
 from dev import bayesLib as bl
+from calibration import lla2eceflib as llef
 
-from multiprocess import Process, Queue
+#from multiprocess import Process, Queue
 # https://github.com/uqfoundation/multiprocess/tree/master/py3.6/examples
 
 # %% LOAD DATA
@@ -71,86 +72,22 @@ lons = calibPts[:,3]
 # -34.629344, -58.370350
 y0, x0 = -34.629344, -58.370350
 z0 = 15.7 # metros, as measured by oliva
-elev = 7 # altura msnm en parque lezama segun google earth
+
 # EGM96 sobre WGS84: https://geographiclib.sourceforge.io/cgi-bin/GeoidEval?input=-34.629344%2C+-58.370350&option=Submit
 geoideval = 16.1066
-h = elev + geoideval # altura dle terreno sobre WGS84
+elev = 7 # altura msnm en parque lezama segun google earth
+h = elev + geoideval # altura del terreno sobre WGS84
 
+# lat lon alt of the data points
+hvec = np.zeros_like(calibPts[:,3]) + h
+LLA = np.concatenate((calibPts[:,2:], hvec.reshape((-1,1))), axis=1)
+LLA0 = np.array([y0, x0, h]) # frame of reference origin
+LLAcam = np.array([y0, x0, h + z0]) # a priori position of camera
 
 
 # %%
-a = 6378137
-f = 1 /298.257223563
-b = a * (1 - f) # 6356752.31424518
-e = np.sqrt(a** - b**2) / a
-ep = e * a / b
-
-# LLA 2 ECEF
-# phi es latitude 
-# lambda es longitud
-# h heigth above ellipsoid (meters)
-# N/rad ardius of curvatiure meters
-
-def lla2ecef(lat, lon, alt):
-    '''
-    convierte lat lon y elevacion sobre WGS a ECEF
-    
-    fuente: 
-        
-    '''
-    la = np.deg2rad(lat)
-    lo = np.deg2rad(lon)
-    cla = np.cos(la)
-    sla = np.sin(la)
-    clo = np.cos(lo)
-    slo = np.sin(lo)
-    
-    rad = a / np.sqrt(1 - e**2 * sla**2)
-    
-    r = rad + alt
-    
-    X = r * cla * clo
-    Y = r * cla * slo
-    Z = (b**2 * rad / a**2 + alt) * sla
-    
-    return np.array([X, Y, Z]).T
-
-# convierto a ECEF en metros
-XYZ = lla2ecef(lats, lons, h)
-xyzOrig = lla2ecef(y0, x0, h) # el origen de coords del mapa
-xyz0 = lla2ecef(y0, x0, h + z0)
-
-# %% Ahora saco los vectores para proyectar esto a un plano tanghente local
-theta = np.deg2rad(x0) # longitud desde el eje x horizontal
-fi = np.pi / 2 - np.deg2rad(y0) # decliancion desde el eje z
-
-ct = np.cos(theta)
-st = np.sin(theta)
-cf = np.cos(fi)
-sf = np.sin(fi)
-
-'''
-defiuno los versores este oeste y altura
-http://mathworld.wolfram.com/SphericalCoordinates.html
-'''
-# versor en la direccion radial
-rVersor = xyzOrig / ln.norm(xyzOrig)
-# e direccion este, versor theta
-eastVersor = np.array([-st, ct, 0])
-# versor norte, opueto el versor fi
-northVersor = - np.array([ct * cf, st * cf, -sf])
-
-# resto el origen
-deltaXYZ = XYZ - xyzOrig
-delta0 = xyz0 - xyzOrig
-
-# proyecto sobre la rotacion que interesa
-# matriz de rotacion para post multiplicar
-R = np.array([eastVersor, northVersor, rVersor]).T
-
-xyzPts = deltaXYZ.dot(R)
-xyzCam =  delta0.dot(R)
-
+xyzPts = llef.lla2ltp(LLA, LLA0)
+xyzCam = llef.lla2ltp(LLAcam, LLA0)
 
 plt.plot(xyzPts[:,0], xyzPts[:,1], '+')
 plt.plot(xyzCam[0], xyzCam[1], 'x')
