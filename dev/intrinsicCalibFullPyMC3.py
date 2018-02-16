6f23ed87064d51ca762d84dfb19745c35b74cc8b
sebalander <sebastian.arroyo@unq.edu.ar>
2018-02-16 19:18:52 -0300

    pymc working, but slow

--- dev/intrinsicCalibFullPyMC3.py
+++ dev/intrinsicCalibFullPyMC3.py
@@ -23,6 +23,8 @@ from copy import deepcopy as dc
 from calibration import calibrator as cl
 import corner
 import time
+
+# %env THEANO_FLAGS='device=cuda, floatX=float32'
 import theano
 import theano. tensor as T
 import pymc3 as pm
@@ -106,7 +108,7 @@ intrinsicParamsOutFile = intrinsicParamsOutFile + str(stdPix) + ".npy"
 
 # pruebo con un par de imagenes
 for j in range(0,n,3):
-    
+
     xm, ym, Cm = cl.inverse(imagePoints[j,0], rVecs[j], tVecs[j], cameraMatrix,
                             distCoeffs, model, Cccd=Ci[j], Cf=False, Ck=False,
                             Crt=False, Cfk=False)
@@ -143,27 +145,28 @@ class ProjectionT(theano.Op):
 
     # Python implementation:
     def perform(self, node, inputs_storage, output_storage):
+        print('Perform a projection')
         Xint, Xext = inputs_storage
         xyM, cM = output_storage
-        
+
         # saco los parametros de flat para que los use la func de projection
         # print(Xint)
         cameraMatrix, distCoeffs = bl.flat2int(Xint, Ns, model)
         # print(cameraMatrix, distCoeffs)
         xy = np.zeros((n,m,2))
         cm = np.zeros((n,m,2,2))
-        
+
         for j in range(n):
             rVec, tVec = bl.flat2ext(Xext[j])
             xy[j,:,0], xy[j,:,1], cm[j] = cl.inverse(imagePoints.reshape((n,m,2))[j], rVec, tVec,
                                     cameraMatrix, distCoeffs, model,
                                     Cccd=Ci[j])
-            
+
         # print(xy)
-        
+
         xyM[0] = xy
         cM[0] = cm
-        
+
     # optional:
     check_input = True
 
@@ -209,73 +212,146 @@ projTheanoWrap = ProjectionT()
 
 # set lower and upper bounds for uniform prior distributions
 # for camera matrix is important to avoid division by zero
-intrLow = [300, 300, 600,  300, -0.1, -0.1, -0.1, -0.1] # intrinsic
+intrLow = [300, 300, 600,  300, -0.1, -0.1, -0.1, -0.1]  # intrinsic
 intrUpp = [500, 500, 1000, 600,  0.1,  0.1,  0.1,  0.1]
 extrLow = np.array([[-3.2, -3.2, -3.2, -25, -25, -25]]*n)
-extrUpp = np.array([[ 3.2,  3.2,  3.2,  25,  25,  25]]*n)
+extrUpp = np.array( [[3.2,  3.2,  3.2,  25,  25,  25]]*n)
+
+allLow = np.concatenate([intrLow, extrLow.reshape(-1)])
+allUpp = np.concatenate([intrUpp, extrUpp.reshape(-1)])
+
+xAll0 = np.concatenate([Xint, XextList.reshape(-1)],0)
 
 with projectionModel:
-    # Priors for unknown model parameters
-    xIn = pm.Uniform('xIn', lower=intrLow, upper=intrUpp, shape=Xint.shape)
-    xEx = pm.Uniform('xEx', lower=extrLow, upper=extrUpp, shape=XextList.shape)
-    
+#    # Priors for unknown model parameters
+#    xIn = pm.Uniform('xIn', lower=intrLow, upper=intrUpp, shape=Xint.shape)
+#    xEx = pm.Uniform('xEx', lower=extrLow, upper=extrUpp, shape=XextList.shape)
+
+    xAll = pm.Uniform('xAll', lower=allLow, upper=allUpp, shape=allLow.shape)
+
+    xIn = xAll[:Ns[-1]]
+    xEx = xAll[Ns[-1]:].reshape((n,6))
+
     xyM, cM = projTheanoWrap(xIn, xEx)
-    
+
     mu = T.reshape(xyM, (-1,))
-    
+
     # sp.block_diag(out[1].reshape((-1,2,2))) # for sparse
     bigC = T.zeros((nTot, nTot))
-    c3Diag = T.reshape(cM, (-1,2,2)) # list of 2x2 covariance blocks
+    c3Diag = T.reshape(cM, (-1, 2, 2)) # list of 2x2 covariance blocks
     bigC = T.set_subtensor(bigC[xInxs, yInxs], c3Diag)
-    
+
     Y_obs = pm.MvNormal('Y_obs', mu=mu, cov=bigC, observed=yObs)
 
 
 
-
-
-
-
-# %%
-# aca saco el maximo a posteriori, bastante util para hacer montecarlo despues
-import scipy.optimize as opt
-
+## %% avoid map estimate as reccomended in
+    # https://discourse.pymc.io/t/frequently-asked-questions/74
+## aca saco el maximo a posteriori, bastante util para hacer montecarlo despues
+#import scipy.optimize as opt
 #
-#try:
-#    map_estimate
-#except:
-#    print('set initial state arbitrarily')
-#    start = {'xIn': Xint,
-#             'xEx': XextList}
-#else:
-#    print('set initial state with previous map_estiamte')
-#    start=map_estimate
+##
+##try:
+##    map_estimate
+##except:
+##    print('set initial state arbitrarily')
+##    start = {'xIn': Xint,
+##             'xEx': XextList}
+##else:
+##    print('set initial state with previous map_estiamte')
+##    start=map_estimate
+#
+#start = {'xIn': Xint, 'xEx': XextList}
+#
+#
+#
+##niter = 1000
+##map_estimate = pm.find_MAP(model=projectionModel, start=start, maxeval=int(niter * 10), maxiter=niter, fmin=opt.fmin, xtol=1e-2,ftol=1e-3)
+#
+#
+#map_estimate = pm.find_MAP(model=projectionModel, start=start)
+#
+#
+#
+#print(map_estimate['xIn']- Xint)
+##print(map_estimate['x0'], x0True)
 
-start = {'xIn': Xint, 'xEx': XextList}
 
 
+# %% metropolis desde MAP. a ver si zafo de la proposal dist
+'''
+http://docs.pymc.io/api/inference.html
+'''
 
-niter = 10
-#map_estimate = pm.find_MAP(model=projectionModel, start=start, maxeval=int(niter * 10), maxiter=niter, fmin=opt.fmin, xtol=1e-2,ftol=1e-3)
+#start = map_estimate
+#start = {'xIn': Xint, 'xEx': XextList}
+start = {'xAll' : xAll0}
+start = {'xAll' : Smean}
 
+#start = {'x0': map_estimate['x0']}#,
+#         'alfa': map_estimate['alfa'],
+#         'rtV': map_estimate['rtV']}
 
-map_estimate = pm.find_MAP(model=projectionModel, start=start, maxeval=niter)
+#scale = {'x0': map_estimate['x0_interval__'],
+#         'alfa': map_estimate['alfa_interval__'],
+#         'rtV': map_estimate['rtV_interval__']}
+#
+#scale = [map_estimate['x0_interval__'], map_estimate['alfa_interval__'], map_estimate['rtV_interval__']]
 
+nDraws = 1000
+nChains = 8
+#
+#Sproposal = np.concatenate([XextList.reshape((-1)), Xint.reshape((-1))])
+#Sproposal = np.abs(Sproposal) * 1e-3
+#
+#Sproposal = {'xIn_interval__' : np.abs(Xint) * 1e-3,
+#             'xEx_interval__' : np.abs(XextList) * 1e-3}
 
+Sproposal = allUpp - allLow
 
-print(map_estimate['xIn'], Xint)
-#print(map_estimate['x0'], x0True)
+with projectionModel:
+    step = pm.Metropolis(S=Scov, scaling=1e-1)
+#    step = pm.Metropolis(vars=basic_model.x0, # basic_model.alfa,basic_model.rtV],
+#                         S=np.abs(map_estimate['x0_interval__'])),
+#                         scaling=1e-1,
+#                         tune=True,
+#                         tune_interval=50)
+
+#    step = pm.Metropolis()
+
+    trace = pm.sample(draws=nDraws, step=step, start=start, njobs=nChains, tune=20,
+                      chains=nChains, progressbar=True) # , live_plot=True,
+#                      compute_convergence_checks=True) #,
+#                      init='auto', n_init=200,  random_seed=123)
+
 
 
 # %%
+plt.figure()
+plt.plot(trace['xAll'] - trace['xAll'][-1])
+
+Scov = np.cov(trace['xAll'].T)
+
+sVals = np.linalg.svd(Scov)[1]
+
+Scov += np.eye(sVals.shape[0]) * sVals[-1]
+Smean = np.mean(trace['xAll'], axis=0)
 
 
+# %%
+plt.figure()
+plt.plot(trace['alfa'])
 
+plt.hist(trace['alfa'], 20)
+plt.hist(trace['x0'], 100)
 
+plt.figure()
+plt.plot(trace['rtV'].reshape(-1,nIm*4))
 
+plt.figure()
+plt.plot(trace['x0'][:,0], trace['x0'][:,1])
 
-
-
+plt.hist2d(trace['x0'][:,0], trace['x0'][:,1], 100)
 
 
 
@@ -311,19 +387,19 @@ class sampleadorExtrinsecoNormal:
     def rvs(self, retPdf=False):
         x = sts.multivariate_normal.rvs(size=(len(self.matrixTransf),6))
         x2 = (x.reshape(-1,6,1) * self.matrixTransf).sum(2)
-        
+
         if retPdf:
             pdf = np.exp(- np.sum(x**2) / 2) / self.piConst
             return x2 + self.mean, pdf
-        
+
         return x2 + self.mean
-    
+
     def setCov(self, covarList):
         self.cov = np.array(covarList)
         self.matrixTransf = np.array([cl.unit2CovTransf(x) for x in self.cov])
 
 
-# %% 
+# %%
 '''
 se propone una pdf de donde samplear. se trata de visualizar y de hacer un
 ajuste grueso
@@ -383,38 +459,38 @@ for i in range(5000):
     Xint1 = sampleadorInt.rvs()
     Xext1 = sampleadorExt.rvs()
     Xerr1 = etotal(Xint1, Ns, Xext1, params)
-    
+
     if Xerr0 > Xerr1: # caso de que dé mejor
         deltaInt = deltaInt * beta + beta1 * (Xint1 - Xint0)
         deltaExt = deltaExt * beta + beta1 * (Xext1 - Xext0)
         print('a la primera', np.linalg.norm(deltaInt), np.linalg.norm(deltaExt))
-        
+
         # salto a ese lugar
         Xint0 = dc(Xint1)
         Xext0 = dc(Xext1)
         Xerr0 = dc(Xerr1)
-        
+
         sampleadorInt.mean = Xint0  + deltaInt
         sampleadorExt.mean = Xext0 + deltaExt
         sampleIntList.append(Xint0)
         sampleExtList.append(Xext0)
         sampleErrList.append(Xerr0)
-        
+
     else: # busco para el otro lado a ver que dá
         Xint2 = 2 * Xint0 - Xint1
         Xext2 = 2 * Xext0 - Xext1
         Xerr2 = etotal(Xint2, Ns, Xext2, params)
-        
+
         if Xerr0 > Xerr2: # caso de que dé mejor la segunda opcion
             deltaInt = deltaInt * beta + beta1 * (Xint2 - Xint0)
             deltaExt = deltaExt * beta + beta1 * (Xext2 - Xext0)
             print('a la segunda', np.linalg.norm(deltaInt), np.linalg.norm(deltaExt))
-            
+
             # salto a ese lugar
             Xint0 = dc(Xint2)
             Xext0 = dc(Xext2)
             Xerr0 = dc(Xerr2)
-            
+
             sampleadorInt.mean = Xint0 + deltaInt
             sampleadorExt.mean = Xext0 + deltaExt
             sampleIntList.append(Xint0)
@@ -430,12 +506,12 @@ for i in range(5000):
                 sampleadorInt.cov *= 1.5  # agrando covarianzas
                 sampleadorExt.setCov(sampleadorExt.cov * 1.5)
                 continue # empiezo loop nuevo
-            
+
             # calculo un nuevo lugar como vertice de la parabola en 1D
             Xint3 = Xint0 + (Xint1 - Xint0) * r
             Xext3 = Xext0 + (Xext1 - Xext0) * r
             Xerr3 = etotal(Xint3, Ns, Xext3, params)
-            
+
             # trato de usar el dato de curvatura para actualizar la covarianza
             diffVectIntr = Xint1 - Xint0
             distInt = np.linalg.norm(diffVectIntr)
@@ -445,8 +521,8 @@ for i in range(5000):
             # saco la curvatura forzando que sea positiva (en gral es)
             a = np.abs((Xerr1 + Xerr2 - 2 * Xerr0)) / distInt**2
             Hintr = a * np.linalg.inv(XtX) # hessiano intrinseco
-            
-            
+
+
             diffVectExtr = Xext1 - Xext0
             distExt = np.linalg.norm(diffVectExtr, axis=1)
             # saco la curvatura forzando que sea positiva (en gral es)
@@ -454,23 +530,23 @@ for i in range(5000):
             XtX = diffVectExtr.reshape((n,6,1)) * diffVectExtr.reshape((n,1,6))
             XtX[:,[0,1,2,3,4,5],[0,1,2,3,4,5]] += distExt.reshape((-1,1)) * 1e-3
             Hextr = a.reshape((-1,1,1)) * np.linalg.inv(XtX) # hessiano intrinseco
-            
+
             # actualizo las covarianzas
             sampleadorInt.cov = sampleadorInt.cov * beta + beta1 * Hintr
             sampleadorExt.setCov(sampleadorExt.cov * beta + beta1 * Hextr)
-            
-            
-            
+
+
+
             if Xerr0 > Xerr3:
                 deltaInt = deltaInt * beta + beta1 * (Xint3 - Xint0)
                 deltaExt = deltaExt * beta + beta1 * (Xext3 - Xext0)
                 print('a la tercera', np.linalg.norm(deltaInt), np.linalg.norm(deltaExt))
-                
+
                 # salto a ese lugar
                 Xint0 = dc(Xint3)
                 Xext0 = dc(Xext3)
                 Xerr0 = dc(Xerr3)
-                
+
                 sampleadorInt.mean = Xint0 + deltaInt
                 sampleadorExt.mean = Xext0 + deltaExt
                 sampleIntList.append(Xint0)
@@ -480,10 +556,10 @@ for i in range(5000):
                 print('no anduvo, probar de nuevo corrigiendo', r)
                 sampleadorInt.cov *= 0.9  # achico covarianzas
                 sampleadorExt.setCov(sampleadorExt.cov * 0.9)
-                
+
                 deltaInt *= 0.7 # achico el salto para buscar mas cerca
                 deltaExt *= 0.7
-                
+
                 sampleadorInt.mean = Xint0 + deltaInt
                 sampleadorExt.mean = Xext0 + deltaExt
 
@@ -609,14 +685,14 @@ tiempoIni = time.time()
 
 for i in range(1, Nmuestras):
     sampleIntList[i], sampleExtList[i], sampleErrList[i], pb = nuevo(sampleIntList[i-1], sampleExtList[i-1], sampleErrList[i-1], retPb=True)
-    
+
     if np.isnan(pb):
         break
-    
+
     # actualizo centroide
     sampleadorInt.mean = sampleIntList[i]
     sampleadorExt.mean = sampleExtList[i]
-    
+
     tiempoNow = time.time()
     Dt = tiempoNow - tiempoIni
     frac = i / Nmuestras # (i  + Nmuestras * j)/ nTot
