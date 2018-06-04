ac46b4c0c883ed24271a6fc216dc9c64c0f02a2b
sebalander <sebastian.arroyo@unq.edu.ar>
2018-06-04 20:14:38 -0300

    namedTouple of all data

--- dev/synthDataGen.py
+++ dev/synthDataGen.py
@@ -19,83 +19,60 @@ from scipy.spatial import Delaunay
 
 
 
-'''
-Fields with default values:
-
-    model = 'stereographic' string indicating camera intrinsic model
-            ['poly', 'rational', 'fisheye', 'stereographic']
-
-    camera = 'vcaWide' string camera model
-
-    s = np.array([1600, 900]) is the image size
-
-    k = 800 is the sintehtic stereographic parameter
-
-    uv= np.array([800, 450]) is the stereographic optical center
-
-    cornersIn = None numpy array that holds the detected corners in
-                chessboard images
-
-    chessModel = None numpy array of the chessboard model, 3D
-
-    cornersEx = None numpy array of the real world corners in urban setting
-
-    objExtr = None numpy array of corresponding lat-lon (or whatever unit)
-              of world calibration points.
-
-    poseRvecs = [] los rVecs asociados a los angulos de las poses
-            sinteticas para las calibraciones extrinsecas simuladas
-
-    poseTvecs = [] los tVecs asociados a angulos y alturas de las poses
-            sinteticas para las calibraciones extrinseas
-            poseTvecs[i,j] es el tVec del angulo i-esimo y altura j-esima
+header = '''Nested namedtuples that hold the data for the paper
 
 Data
     Synt
-        Intr         # listo: SynthIntr
-            k
-            uv
-            camera
-            model
-            s
-        Ches         # listo: SynthChes
-            nIm
-            nPt
-            objPt
-            imgPt
-            rVecs
-            tVecs
-        Extr         # listo: SynthExtr
-            ang
-            h
-            rVecs
-            tVecs
-            objPt
-            imgPt
-            index10
+        Intr         # listo: SyntIntr
+            camera  'vcaWide' string camera model
+            model   string indicating camera intrinsic model
+                    ['poly', 'rational', 'fisheye', 'stereographic']
+            s       is the image size
+            k       sintehtic stereographic parameter
+            uv      = s / 2 is the stereographic optical center
+        Ches         # listo: SyntChes
+            nIm     number of images
+            nPt     number of point in image
+            objPt   chessboard model grid
+            rVecs   synth rotation vectors
+            tVecs   synth tVecs
+            imgPt   synth corners projected from objPt with synth params
+        Extr         # listo: SyntExtr
+            ang     angles of synth pose tables
+            h       heights  of synth pose tables
+            rVecs   rotation vectors associated to angles
+            tVecs   tVecs associated to angles and h
+            objPt   distributed 3D points on the floor
+            imgPt   projected to image
+            index10 indexes to select 10 points well distributed
     Real
         Ches         # listo: RealChes
-            nIm
-            nPt
-            objPt
-            imgPt
-            imgFiles
+            nIm     number of chess images
+            nPt     number of chess points per image
+            objPt   chessboard model, 3D
+            imgPt   detected corners in chessboard images
+            imgFls  list of paths to the chessboard images
         Balk
-            objPt
-            imgPt
+            objPt   calibration world points, lat lon
+            imgPt   image points for calibration
+            priLLA  prior lat-lon-altura
+            imgFl   camera snapshot file
         Dete
+            carGps  car gps coordinates
+            carIm   car image detection traces
+
 '''
 
-synthintr = clt.namedtuple('synthintr', ['k', 'uv', 's', 'camera', 'model'])
-synthches = clt.namedtuple('synthches', ['nIm', 'nPt', 'objPt', 'imgPt',
-                                      'rVecs', 'tVecs'])
-synthextr = clt.namedtuple('synthextr', ['ang', 'h', 'rVecs', 'tVecs', 'objPt',
-                                    'imgPt', 'index10'])
+syntintr = clt.namedtuple('syntintr', ['k', 'uv', 's', 'camera', 'model'])
+syntches = clt.namedtuple('syntches', ['nIm', 'nPt', 'objPt', 'imgPt',
+                                       'rVecs', 'tVecs'])
+syntextr = clt.namedtuple('syntextr', ['ang', 'h', 'rVecs', 'tVecs', 'objPt',
+                                       'imgPt', 'index10'])
 synt = clt.namedtuple('synt', ['Intr', 'Ches', 'Extr'])
 
-realches = clt.namedtuple('realches', ['nIm', 'nPt', 'objPt', 'imgPt', 'imgFiles'])
-realbalk = clt.namedtuple('realbalk', ['objPt', 'imgPt'])
-realdete = clt.namedtuple('realdete', [])
+realches = clt.namedtuple('realches', ['nIm', 'nPt', 'objPt', 'imgPt', 'imgFls'])
+realbalk = clt.namedtuple('realbalk', ['objPt', 'imgPt', 'priorLLA', 'imgFl'])
+realdete = clt.namedtuple('realdete', ['carGPS', 'carIm'])
 real = clt.namedtuple('real', ['Ches', 'Balk', 'Dete'])
 
 datafull = clt.namedtuple('datafull', ['Synt', 'Real'])
@@ -106,11 +83,11 @@ datafull = clt.namedtuple('datafull', ['Synt', 'Real'])
 
 camera = 'vcaWide'
 model='stereographic'
-s = np.array((1600, 900))
+s = np.array((1600, 904))
 uv = s / 2.0
 k = 800.0  # a proposed k for the camera, en realidad estimamos que va a ser #815
 
-SynthIntr = synthintr(k, uv, s, camera, model)
+SyntIntr = syntintr(k, uv, s, camera, model)
 
 
 
@@ -154,17 +131,18 @@ cameraMatrix = np.eye(3)
 cameraMatrix[[0,1],2] = uv
 distCoeff = np.array([k])
 
-SynthChesImPts = np.array([cl.direct(ChesObjPt, SyntChesRvecs[i],
+SyntChesImPts = np.array([cl.direct(ChesObjPt, SyntChesRvecs[i],
                                      SyntChesTvecs[i], cameraMatrix, distCoeff,
                                      model) for i in range(ChesnIm)])
 
-SynthChes = synthches(ChesnIm, ChesnPt, ChesObjPt, SynthChesImPts,
+SyntChes = syntches(ChesnIm, ChesnPt, ChesObjPt, SyntChesImPts,
                       SyntChesRvecs, SyntChesTvecs)
 
 plt.figure()
-plt.scatter(SynthChes.imgPt[:, :, 0], SynthChes.imgPt[:, :, 1])
-
-
+plt.title('corners de todas las imagenes')
+for crnr in SyntChes.imgPt:
+    plt.scatter(crnr[:, 0], crnr[:, 1], marker='x', s=5)
+plt.axis('equal')
 
 # %% =========================================================================
 #  SYNTHETIC EXTRINSIC table of poses sintéticas
@@ -187,18 +165,17 @@ yC = np.array([zC[0], zC[2], - zC[1]])
 Rmats = np.concatenate([[xC], [yC], [zC]]).transpose((2,1,0))
 
 # calculos los rVecs
-SynthExtrRvecs = np.array([cv2.Rodrigues(R)[0] for R in Rmats]).reshape((-1, 3))
+SyntExtrRvecs = np.array([cv2.Rodrigues(R)[0] for R in Rmats]).reshape((-1, 3))
 
 # los tVecs los saco de las alturas
 T0 = np.zeros((3,2))
 T0[2] = hs
 
 # son 6 vectores tVecs[i, j] es el correspondiente al angulo i y altura j
-SynthExtrTvecs = np.transpose(- Rmats.dot(T0), (0, 2, 1))
+SyntExtrTvecs = np.transpose(- Rmats.dot(T0), (0, 2, 1))
 
 
 # %%
-
 
 # radios en el piso
 np.tan(thMax - angs).reshape((1, -1)) * hs.reshape((-1, 1))
@@ -247,16 +224,16 @@ tri = Delaunay(mu)
 
 
 
-plt.subplot(131)
-plt.imshow(areInCircle)
-
-plt.subplot(132)
-plt.scatter(xy[0], xy[1], c=labels, cmap='tab20')
-plt.axis('equal')
-plt.scatter(mu[:, 0], mu[:, 1], marker='x', s=10, c='k')
-plt.triplot(mu[:,0], mu[:,1], tri.simplices)
-for i in range(Npts[1]):
-    plt.text(mu[i, 0], mu[i, 1], i)
+#plt.subplot(131)
+#plt.imshow(areInCircle)
+#
+#plt.subplot(132)
+#plt.scatter(xy[0], xy[1], c=labels, cmap='tab20')
+#plt.axis('equal')
+#plt.scatter(mu[:, 0], mu[:, 1], marker='x', s=10, c='k')
+#plt.triplot(mu[:,0], mu[:,1], tri.simplices)
+#for i in range(Npts[1]):
+#    plt.text(mu[i, 0], mu[i, 1], i)
 
 
 # para imprimir los vecinos del nodo 6
@@ -271,57 +248,79 @@ def distortionNodes(indSel):
 
 # indexes to select from 20 to 10
 indSel = np.sort([1,  3,  4,  5,  8,  9, 10, 11, 12, 16])
-mu10, distor = distortionNodes(indSel)
 
+def optimizeIndSel(indSel, verbose=False):
+    mu10, distor = distortionNodes(indSel)
 
-# recorro los indices buscando en cada caso el cambio mejor a un vecino 1o
-nCambs = -1
-loops = -1
-loopsMax = 100
+    # recorro los indices buscando en cada caso el cambio mejor a un vecino 1o
+    nCambs = -1
+    loops = -1
+    loopsMax = 100
 
-distorEvol = list()
-distorEvol.append(distor)
+    distorEvol = list()
+    distorEvol.append(distor)
 
-while nCambs is not 0 and loops < loopsMax:
-    nCambs = 0
-    loops += 1
-    print('Empezamos un loop nuevo ', loops)
-    print('==========================')
-    for ii, ind in enumerate(indSel):
-        vec = vecinos(ind)
-        vec = vec[[v not in indSel for v in vec]]
+    while nCambs is not 0 and loops < loopsMax:
+        nCambs = 0
+        loops += 1
+        if verbose:
+            print('Empezamos un loop nuevo ', loops)
+            print('==========================')
+        for ii, ind in enumerate(indSel):
+            vec = vecinos(ind)
+            vec = vec[[v not in indSel for v in vec]]
 
-        indSelList = np.array([indSel]*(len(vec)))
-        indSelList[:, ii] = vec
+            if len(vec) is 0:
+                continue
 
-        distorList = [distortionNodes(indVec)[1] for indVec in indSelList]
+            indSelList = np.array([indSel] * len(vec))
+            indSelList[:, ii] = vec
 
-        if np.all(distor < distorList):
-            # me quedo con la que estaba
-            print('\tno hay cambio', ii)
-        else:
-            imin = np.argmin(distorList)
-            indSel = np.sort(np.copy(indSelList[imin]))
-            distor = distorList[imin]
-            distorEvol.append(distor)
-            nCambs += 1
-            print('\thay cambio, es el ', nCambs, 'elemento', ii)
+            distorList = [distortionNodes(indVec)[1] for indVec in indSelList]
 
+            if np.all(distor < distorList):
+                # me quedo con la que estaba
+                if verbose:
+                    print('\tno hay cambio', ii)
+            else:
+                imin = np.argmin(distorList)
+                indSel = np.sort(np.copy(indSelList[imin]))
+                distor = distorList[imin]
+                distorEvol.append(distor)
+                nCambs += 1
+                if verbose:
+                    print('\thay cambio, es el ', nCambs, 'elemento', ii)
 
-print(indSel)
-mu10, distor = distortionNodes(indSel)
+    if verbose:
+        print(indSel)
+    mu10, distor = distortionNodes(indSel)
+    return indSel, mu10, distor
 
+nIter = 50
+indList20 = np.arange(0, 20)
 
+retOpt = list()
+for i in range(nIter):
+    indSel = np.random.choice(indList20, 10, replace=False)
+    retOpt.append(optimizeIndSel(indSel, False))
 
-plt.subplot(133)
+argBest = np.argmin([ret[2] for ret in retOpt])
+
+indSel, mu10, distor = retOpt[argBest]
+
+#plt.subplot(133)
+plt.figure()
+plt.title('distribución en el espacio imagen')
 code, dist = vq(data, mu10)
 plt.scatter(xy[0], xy[1], c=code, cmap='tab20')
-plt.scatter(mu10[:, 0], mu10[:, 1], marker='<', s=50, c='k')
 plt.axis('equal')
-plt.triplot(mu[:,0], mu[:,1], tri.simplices)
 for i in range(Npts[1]):
-    plt.text(mu[i, 0], mu[i, 1], i)
+    plt.text(mu[i, 0] + 0.05, mu[i, 1] + 0.05, i)
+plt.triplot(mu[:,0], mu[:,1], tri.simplices, c='k')
+plt.scatter(mu10[:, 0], mu10[:, 1], marker='<', s=100, c='w')
 
+
+# %%
 
 # ahora los convierto al plano del mundo
 # rMaxW
@@ -337,22 +336,20 @@ rW = hs[0] * np.tan(thW)
 muW = (muIm.T * rW / rIm).T
 muW10 = muW[indSel]
 
-SynthExtrObjPt = np.concatenate([muW, np.zeros((muW.shape[0],1))], axis=1)
+SyntExtrObjPt = np.concatenate([muW, np.zeros((muW.shape[0],1))], axis=1)
 
 
 # proyecto a la imagen
-SynthExtrImPt = np.zeros((len(angs), len(hs), muW.shape[0], 2))
+SyntExtrImPt = np.zeros((len(angs), len(hs), muW.shape[0], 2))
 for i in range(len(angs)):
-    rv = SynthExtrRvecs[i]
+    rv = SyntExtrRvecs[i]
     for j in range(len(hs)):
-        tv = SynthExtrTvecs[i, j]
-        SynthExtrImPt[i, j] = cl.direct(SynthExtrObjPt, rv, tv, cameraMatrix,
+        tv = SyntExtrTvecs[i, j]
+        SyntExtrImPt[i, j] = cl.direct(SyntExtrObjPt, rv, tv, cameraMatrix,
                                         distCoeff, model)
 
-SynthExtr = synthextr(angs, hs, SynthExtrRvecs, SynthExtrTvecs, muW,
-                      SynthExtrImPt, indSel)
-
-
+SyntExtr = syntextr(angs, hs, SyntExtrRvecs, SyntExtrTvecs, muW,
+                      SyntExtrImPt, indSel)
 
 
 # %%
@@ -361,19 +358,68 @@ from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-ax.scatter(muW[:, 0], muW[:, 1], 0, c='r')
-ax.scatter(muW10[:, 0], muW10[:, 1], 0, marker='>', c='k', s=200)
+ax.scatter(SyntExtrObjPt[:, 0], SyntExtrObjPt[:, 1], 0, c='r', s=100)
+ax.scatter(SyntExtrObjPt[SyntExtr.index10, 0], SyntExtrObjPt[SyntExtr.index10, 1],
+           0, c='k', s=200)
 
-ax.scatter([0,0], [0, 0], hs, marker='X')
+cols = ['k', 'b', 'm', 'g', 'r', 'c', 'y',]
+
+for i in range(len(SyntExtr.h)):
+    for j in range(len(SyntExtr.ang)):
+        rMat = cv2.Rodrigues(SyntExtr.rVecs[j])[0]
+        tVw = - rMat.T.dot(SyntExtr.tVecs[j, i])
+
+        print(tVw, np.rad2deg(np.arccos(rMat[:,2].dot([0,0,-1]))))
+        for ve in 3 * rMat.T:
+            ax.plot([tVw[0], tVw[0] + ve[0]],
+                    [tVw[1], tVw[1] + ve[1]],
+                    [tVw[2], tVw[2] + ve[2]], c=cols[j])
 
 
 for i in range(Npts[1]):
     ax.text(muW[i, 0], muW[i, 1], 0, s=i)
 
-# %%
+# %% =========================================================================
+# DATOS EXTRINSECOS
+
+# puntos da calibracion sacadas
+calibPointsFile = "./resources/nov16/puntosCalibracion.txt"
+imageFile = "./resources/nov16/vlcsnap.png"
+calibPts = np.loadtxt(calibPointsFile)
+
+priorLLA = np.array([-34.629344, -58.370350, 15.7])
+
+RealBalk = realbalk(calibPts[:,2:], calibPts[:,:2], priorLLA, imageFile)
+
+plt.figure()
+plt.scatter(RealBalk.objPt[:, 1], RealBalk.objPt[:, 0])
+plt.scatter(RealBalk.priorLLA[1], RealBalk.priorLLA[0])
+plt.axis('equal')
 
 
 
+# %% detecciones
+gpsCelFile = "/home/sebalander/Code/sebaPhD/resources/encoderGPS/"
+gpsCelFile += "20161113192738.txt"
 
+import pandas as pd
 
+gps = pd.read_csv(gpsCelFile)
 
+carGPS = gps.loc[:, ['time', 'lat', 'lon', 'accuracy']].as_matrix()
+
+carIm = []
+RealDete = realdete(carGPS, carIm)
+
+# %% =========================================================================
+# JUNTO TODO Y GUARDO
+Synt = synt(SyntIntr, SyntChes, SyntExtr)
+Real = real(RealChes, RealBalk, RealDete)
+DataFull = datafull(Synt, Real)
+
+fullDataFile = "./resources/fullDataIntrExtr"
+np.save(fullDataFile + ".npy", DataFull)
+
+text_file = open(fullDataFile + "README.txt", "w")
+text_file.write(header)
+text_file.close()
---
+++ resources/fullDataIntrExtrREADME.txt
@@ -0,0 +1,42 @@
+
+Nested namedtuples that hold the data for the paper
+
+Data
+    Synt
+        Intr         # listo: SyntIntr
+            camera  'vcaWide' string camera model
+            model   string indicating camera intrinsic model
+                    ['poly', 'rational', 'fisheye', 'stereographic']
+            s       is the image size
+            k       sintehtic stereographic parameter
+            uv      = s / 2 is the stereographic optical center
+        Ches         # listo: SyntChes
+            nIm     number of images
+            nPt     number of point in image
+            objPt   chessboard model grid
+            rVecs   synth rotation vectors
+            tVecs   synth tVecs
+            imgPt   synth corners projected from objPt with synth params
+        Extr         # listo: SyntExtr
+            ang     angles of synth pose tables
+            h       heights  of synth pose tables
+            rVecs   rotation vectors associated to angles
+            tVecs   tVecs associated to angles and h
+            objPt   distributed 3D points on the floor
+            imgPt   projected to image
+            index10 indexes to select 10 points well distributed
+    Real
+        Ches         # listo: RealChes
+            nIm     number of chess images
+            nPt     number of chess points per image
+            objPt   chessboard model, 3D
+            imgPt   detected corners in chessboard images
+            imgFls  list of paths to the chessboard images
+        Balk
+            objPt   calibration world points, lat lon
+            imgPt   image points for calibration
+            priLLA  prior lat-lon-altura
+            imgFl   camera snapshot file
+        Dete
+            carGps  car gps coordinates
+            carIm   car image detection traces
